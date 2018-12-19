07137229f14cd4454451630090285c2302a2cad0
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2018-12-19 11:02:30 +0000

    config changes

--- server/config.js
+++ server/config.js
@@ -1,34 +1,42 @@
 require('dotenv').config()
+const logger = require('../log.js')
 
 const production = process.env.NODE_ENV === 'production'
 
-function get(name, fallback, options = {}) {
+function get(name, fallback, log, options = {}) {
   if (process.env[name]) {
+    if (log) {
+      logger.info(`Env var: ${name} value: ${process.env[name]}`)
+    }
     return process.env[name]
   }
   if (fallback !== undefined && (!production || !options.requireInProduction)) {
+    if (log) {
+      logger.info(`Env var: ${name} value: ${fallback}`)
+    }
     return fallback
   }
   throw new Error(`Missing env var ${name}`)
 }
 
+
 module.exports = {
-  sessionSecret: get('SESSION_SECRET', 'app-insecure-default-session', { requireInProduction: true }),
+  sessionSecret: get('SESSION_SECRET', 'app-insecure-default-session', false, { requireInProduction: true }),
   db: {
-    username: get('DB_USER', 'form-builder'),
+    username: get('DB_USER', 'form-builder', true),
     password: get('DB_PASS', 'form-builder'),
-    server: get('DB_SERVER', 'localhost'),
-    database: get('DB_NAME', 'form-builder'),
-    sslEnabled: get('DB_SSL_ENABLED', 'false'),
+    server: get('DB_SERVER', 'localhost', true),
+    database: get('DB_NAME', 'form-builder', true),
+    sslEnabled: get('DB_SSL_ENABLED', 'false', true),
   },
   nomis: {
-    authUrl: get('NOMIS_AUTH_URL', 'http://localhost:8080/auth'),
-    authExternalUrl: get('NOMIS_AUTH_EXTERNAL_URL', get('NOMIS_AUTH_URL', 'http://localhost:8080/auth')),
+    authUrl: get('NOMIS_AUTH_URL', 'http://localhost:8080/auth', true),
+    authExternalUrl: get('NOMIS_AUTH_EXTERNAL_URL', get('NOMIS_AUTH_URL', 'http://localhost:8080/auth'), true),
     timeout: {
       response: 30000,
       deadline: 35000,
     },
-    apiClientId: get('API_CLIENT_ID', 'licences'),
+    apiClientId: get('API_CLIENT_ID', 'licences', true),
     apiClientSecret: get('API_CLIENT_SECRET', 'clientsecret'),
   },
   domain: 'http://localhost:3000',
