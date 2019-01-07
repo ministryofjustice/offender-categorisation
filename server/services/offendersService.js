2bf9f69fb8c1812efd47bbd52692da3c4cebd404
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2019-01-07 10:11:32 +0000

    custody api call with client credentials authentication.

--- server/services/offendersService.js
+++ server/services/offendersService.js
@@ -1,26 +1,23 @@
-const logger = require('../../log.js');
-const {isNilOrEmpty} = require('../utils/functionalHelpers');
+const logger = require('../../log.js')
+const { isNilOrEmpty } = require('../utils/functionalHelpers')
 
 module.exports = function createOffendersService(custodyClientBuilder) {
-    async function getOffendersInPrison(user, agencyId) {
-        try {
+  async function getOffendersInPrison(user, agencyId) {
+    try {
+      const custodyClient = custodyClientBuilder(user.username)
+      const offenders = await custodyClient.getOffendersInPrison(agencyId)
 
-            const custodyClient = custodyClientBuilder(user.username);
-            const offenders = await custodyClient.getOffendersInPrison(agencyId);
+      if (isNilOrEmpty(offenders)) {
+        logger.info('No available offenders')
+        return []
+      }
 
-            if (isNilOrEmpty(offenders)) {
-                logger.info('No available offenders');
-                return [];
-            }
-
-            return offenders;
-
-        } catch (error) {
-            logger.error('Error during getOffendersInPrison: ', error.stack);
-            throw error;
-        }
+      return offenders
+    } catch (error) {
+      logger.error('Error during getOffendersInPrison: ', error.stack)
+      throw error
     }
+  }
 
-    return {getOffendersInPrison};
-};
-
+  return { getOffendersInPrison }
+}
--- test/data/custodyClient.test.js
+++ test/data/custodyClient.test.js
@@ -1,37 +1,30 @@
-const nock = require('nock');
+const nock = require('nock')
 
-const config = require('../../server/config');
-const custodyClientBuilder = require('../../server/data/custodyClientBuilder');
+const config = require('../../server/config')
+const custodyClientBuilder = require('../../server/data/custodyClientBuilder')
 
-describe('custodyClient', function() {
-    let fakeCustodyApi;
-    let fakeNomisAuth;
-    let custodyClient;
+describe('custodyClient', () => {
+  let fakeCustodyApi
+  let fakeNomisAuth
+  let custodyClient
 
-    beforeEach(() => {
-        fakeNomisAuth = nock(`${config.apis.oauth2.url}`).log(console.log);
-        fakeCustodyApi = nock(`${config.apis.custody.url}`).log(console.log);
-        custodyClient = custodyClientBuilder('username');
-    });
+  beforeEach(() => {
+    fakeNomisAuth = nock(`${config.apis.oauth2.url}`)
+    fakeCustodyApi = nock(`${config.apis.custody.url}`)
+    custodyClient = custodyClientBuilder('username')
+  })
 
-    afterEach(() => {
-        nock.cleanAll();
-    });
+  afterEach(() => {
+    nock.cleanAll()
+  })
 
-    describe('getOffendersInPrison', () => {
+  describe('getOffendersInPrison', () => {
+    it('should return data from api', async () => {
+      fakeNomisAuth.post(`/oauth/token`).reply(200, { access_token: 'token123' })
+      fakeCustodyApi.get(`/api/offenders/prison/LEI`).reply(200, { key: 'value' })
 
-        it('should return data from api', async () => {
-          fakeNomisAuth
-            .post(`/oauth/token`)
-            .reply(200, {access_token: 'token123'});
-          fakeCustodyApi
-            .get(`/api/offenders/prison/LEI`)
-            .reply(200, {key: 'value'});
-
-          const output = await custodyClient.getOffendersInPrison('LEI')
-            return expect(stuff).toEqual({key: 'value'});
-        });
-    });
-
-});
-
+      const output = await custodyClient.getOffendersInPrison('LEI')
+      return expect(output).toEqual({ key: 'value' })
+    })
+  })
+})
