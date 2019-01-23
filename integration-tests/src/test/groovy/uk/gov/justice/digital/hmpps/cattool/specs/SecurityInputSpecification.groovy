5eb470a2e82b5433f6af5c47030cea29e5b1704f
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2019-01-23 10:11:59 +0000

    DC-83 and DC-84 common header and ratings template html

---
+++ integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/specs/SecurityInputSpecification.groovy
@@ -0,0 +1,46 @@
+package uk.gov.justice.digital.hmpps.cattool.specs
+
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer
+import geb.spock.GebReportingSpec
+import org.junit.Rule
+import uk.gov.justice.digital.hmpps.cattool.mockapis.Elite2Api
+import uk.gov.justice.digital.hmpps.cattool.mockapis.OauthApi
+import uk.gov.justice.digital.hmpps.cattool.model.TestFixture
+import uk.gov.justice.digital.hmpps.cattool.pages.CategoriserHomePage
+import uk.gov.justice.digital.hmpps.cattool.pages.CategoriserTasklistPage
+import uk.gov.justice.digital.hmpps.cattool.pages.CategoriserSecurityInputPage
+
+import java.time.LocalDate
+
+import static uk.gov.justice.digital.hmpps.cattool.model.UserAccount.ITAG_USER
+
+class SecurityInputSpecification extends GebReportingSpec {
+
+  @Rule
+  Elite2Api elite2api = new Elite2Api()
+
+  @Rule
+  OauthApi oauthApi = new OauthApi(new WireMockConfiguration()
+    .extensions(new ResponseTemplateTransformer(false)))
+
+  TestFixture fixture = new TestFixture(browser, elite2api, oauthApi)
+
+  def "The tasklist for a categoriser is present"() {
+    when: 'I go to the tasklist page'
+
+    elite2api.stubUncategorised()
+    elite2api.stubSentenceData(['B2354XY', 'B2354YZ'], [11, 12], LocalDate.now().plusDays(-3).toString())
+
+    fixture.loginAs(ITAG_USER)
+    at CategoriserHomePage
+    elite2api.stubGetUserDetails(12)
+    startButtons[0].click()
+    at(new CategoriserTasklistPage(bookingId: '12'))
+    securityButton.click()
+
+    then: 'The security input page is displayed'
+    at(new CategoriserSecurityInputPage(bookingId: '12'))
+
+  }
+}
