4c580cd15987dc737f47a01577c7596f04b5b676
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2019-02-24 19:16:03 +0000

    persist suggested category and wire up to supervisor review

---
+++ integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/pages/SupervisorReviewOutcomePage.groovy
@@ -0,0 +1,16 @@
+package uk.gov.justice.digital.hmpps.cattool.pages
+
+import geb.Page
+
+class SupervisorReviewOutcomePage extends Page {
+
+  static url = '/form/supervisor/review'
+
+  static at = {
+    headingText == 'Review outcome saved'
+  }
+
+  static content = {
+    headingText { $('h1.govuk-panel__title').text() }
+  }
+}
