4b299f8fb5eec5ed5aa9f2599e0d88ed81565748
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2019-03-20 12:54:13 +0000

    typo

--- server/routes/home.js
+++ server/routes/home.js
@@ -61,6 +61,23 @@ module.exports = function Index({ authenticationMiddleware, userService, offende
   )
 
   router.get(
+    '/supervisorDone',
+    asyncMiddleware(async (req, res) => {
+      const user = await userService.getUser(res.locals.user.token)
+      res.locals.user = { ...user, ...res.locals.user }
+
+      const offenders = res.locals.user.activeCaseLoad
+        ? await offendersService.getCategorisedOffenders(
+            res.locals.user.token,
+            res.locals.user.activeCaseLoad.caseLoadId,
+            user
+          )
+        : []
+      res.render('pages/supervisorDone', { offenders })
+    })
+  )
+
+  router.get(
     '/supervisorHome',
     asyncMiddleware(async (req, res) => {
       const user = await userService.getUser(res.locals.user.token)
