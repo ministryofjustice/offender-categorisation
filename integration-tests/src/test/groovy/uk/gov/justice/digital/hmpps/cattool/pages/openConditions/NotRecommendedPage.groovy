8045fd17f68e0cb6b3f9988524b04f8741d676ef
steverendell <steve.rendell@syscon.net>
2019-04-12 17:00:43 +0100

    DC-165 Build 'Open conditions not recommended' page

---
+++ integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/pages/openConditions/NotRecommendedPage.groovy
@@ -0,0 +1,25 @@
+package uk.gov.justice.digital.hmpps.cattool.pages.openConditions
+
+import geb.Page
+
+class NotRecommendedPage extends Page {
+
+  static url = '/form/openConditions/notRecommended'
+
+  static at = {
+    headingText == 'Open conditions not recommended'
+  }
+
+  static content = {
+    headingText { $('h1.govuk-heading-l').text() }
+    form { $('form') }
+    stillReferYes { $('#stillRefer-1') }
+    stillReferNo { $('#stillRefer-2') }
+
+    reasons { $('ul.govuk-\\!-font-weight-bold').find('li') }
+    submitButton { $('button', type: 'submit') }
+    backLink { $('a.govuk-back-link') }
+    errorSummaries { $('ul.govuk-error-summary__list li') }
+    errors { $('span.govuk-error-message') }
+  }
+}
