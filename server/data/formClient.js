d0039977acbf76a1aa1fa17672af3812656d1858
steverendell <steve.rendell@syscon.net>
2019-05-03 17:03:26 +0100

    DC-263 transactions - implement throughout

--- server/data/formClient.js
+++ server/data/formClient.js
@@ -1,4 +1,3 @@
-const db = require('./dataAccess/db')
 const logger = require('../../log.js')
 
 const sequenceClause =
@@ -22,28 +21,27 @@ module.exports = {
       where f.booking_id = $1 ${sequenceClause}`,
       values: [bookingId],
     }
-    const client = transactionalClient || db
-    return client.query(query)
+    return transactionalClient.query(query)
   },
 
-  getCategorisationRecordsByStatus(agencyId, statusList) {
+  getCategorisationRecordsByStatus(agencyId, statusList, transactionalClient) {
     logger.debug(`getCategorisationRecordsByStatus called for ${agencyId}, status ${statusList}`)
     const query = {
       text: `select id, booking_id as "bookingId", user_id as "userId", status, form_response as "formObject", assigned_user_id as "assignedUserId", referred_date as "securityReferredDate", referred_by as "securityReferredBy", security_reviewed_date as "securityReviewedDate", security_reviewed_by as "securityReviewedBy"
         from form f where f.prison_id = $1 and f.status = ANY ($2) ${sequenceClause}`,
       values: [agencyId, statusList],
     }
-    return db.query(query)
+    return transactionalClient.query(query)
   },
 
-  getSecurityReviewedCategorisationRecords(agencyId) {
+  getSecurityReviewedCategorisationRecords(agencyId, transactionalClient) {
     logger.debug(`getSecurityReviewedOffenders called for ${agencyId}`)
     const query = {
       text: `select id, booking_id as "bookingId", user_id as "userId", status, form_response as "formObject", assigned_user_id as "assignedUserId", referred_date as "securityReferredDate", referred_by as "securityReferredBy", security_reviewed_date as "securityReviewedDate", security_reviewed_by as "securityReviewedBy"
         from form f where f.prison_id = $1 and security_reviewed_date is not null ${sequenceClause}`,
       values: [agencyId],
     }
-    return db.query(query)
+    return transactionalClient.query(query)
   },
 
   referToSecurity(bookingId, userId, status, transactionalClient) {
@@ -52,44 +50,43 @@ module.exports = {
       text: `update form f set status = $1, referred_date = CURRENT_TIMESTAMP, referred_by = $2 where booking_id = $3 ${sequenceClause}`,
       values: [status, userId, bookingId],
     }
-    const client = transactionalClient || db
-    return client.query(query)
+    return transactionalClient.query(query)
   },
 
-  securityReviewed(bookingId, status, userId) {
+  securityReviewed(bookingId, status, userId, transactionalClient) {
     logger.debug(`securityReviewed called for ${userId} with status ${status} and booking id ${bookingId}`)
     const query = {
       text: `update form f set security_reviewed_date = CURRENT_TIMESTAMP, security_reviewed_by = $1, status = $2 where booking_id = $3 ${sequenceClause}`,
       values: [userId, status, bookingId],
     }
-    return db.query(query)
+    return transactionalClient.query(query)
   },
 
-  updateStatus(bookingId, status) {
+  updateStatus(bookingId, status, transactionalClient) {
     logger.debug(`updateStatus called for booking id ${bookingId} and status ${status}`)
     const query = {
       text: `update form f set status = $1 where booking_id = $2 ${sequenceClause}`,
       values: [status, bookingId],
     }
-    return db.query(query)
+    return transactionalClient.query(query)
   },
 
-  updateFormData(bookingId, formResponse) {
+  updateFormData(bookingId, formResponse, transactionalClient) {
     logger.debug(`updateFormData for booking id ${bookingId}`)
     const query = {
       text: `update form f set form_response = $1 where booking_id = $2 ${sequenceClause}`,
       values: [formResponse, bookingId],
     }
-    return db.query(query)
+    return transactionalClient.query(query)
   },
 
-  updateRiskProfileData(bookingId, data) {
+  updateRiskProfileData(bookingId, data, transactionalClient) {
     logger.debug(`mergeRiskProfileData for booking id ${bookingId}`)
     const query = {
       text: `update form f set risk_profile = $1 where booking_id = $2 ${sequenceClause}`,
       values: [data, bookingId],
     }
-    return db.query(query)
+    return transactionalClient.query(query)
   },
 
   update(formId, formResponse, bookingId, status, transactionalClient) {
@@ -98,8 +95,7 @@ module.exports = {
       text: `update form f set form_response = $1, status = $2 where f.booking_id = $3 ${sequenceClause}`,
       values: [formResponse, status, bookingId],
     }
-    const client = transactionalClient || db
-    return client.query(query)
+    return transactionalClient.query(query)
   },
 
   create(bookingId, userId, status, assignedUserId, prisonId, offenderNo, transactionalClient) {
@@ -109,7 +105,6 @@ module.exports = {
         'insert into form (form_response, booking_id, user_id, status, assigned_user_id, sequence_no, prison_id, offender_no, start_date) values ($1, $2, $3, $4, $5, 1, $6, $7, CURRENT_TIMESTAMP)',
       values: [{}, bookingId, userId, status, assignedUserId, prisonId, offenderNo],
     }
-    const client = transactionalClient || db
-    return client.query(query)
+    return transactionalClient.query(query)
   },
 }
--- test/data/formClient.test.js
+++ test/data/formClient.test.js
@@ -1,22 +1,23 @@
 const formClient = require('../../server/data/formClient')
-const db = require('../../server/data/dataAccess/db')
+
+const mockTransactionalClient = { query: jest.fn(), release: jest.fn() }
 
 jest.mock('../../server/data/dataAccess/db')
 
 afterEach(() => {
-  db.query.mockReset()
+  mockTransactionalClient.query.mockReset()
 })
 
 describe('getFormDataForUser', () => {
   test('it should call query on db', () => {
-    formClient.getFormDataForUser('bookingId1')
-    expect(db.query).toBeCalledTimes(1)
+    formClient.getFormDataForUser('bookingId1', mockTransactionalClient)
+    expect(mockTransactionalClient.query).toBeCalledTimes(1)
   })
 
   test('it should pass on the correct sql', () => {
-    formClient.getFormDataForUser('bookingId1')
+    formClient.getFormDataForUser('bookingId1', mockTransactionalClient)
 
-    expect(db.query).toBeCalledWith({
+    expect(mockTransactionalClient.query).toBeCalledWith({
       text: `select id,
                     booking_id             as "bookingId",
                     user_id                as "userId",
@@ -37,9 +38,9 @@ describe('getFormDataForUser', () => {
 
 describe('getCategorisationRecordsByStatus', () => {
   test('it should pass on the correct sql', () => {
-    formClient.getCategorisationRecordsByStatus('MDI', ['APPROVED', 'AWAITING_APPROVAL'])
+    formClient.getCategorisationRecordsByStatus('MDI', ['APPROVED', 'AWAITING_APPROVAL'], mockTransactionalClient)
 
-    expect(db.query).toBeCalledWith({
+    expect(mockTransactionalClient.query).toBeCalledWith({
       text: `select id, booking_id as "bookingId", user_id as "userId", status, form_response as "formObject", assigned_user_id as "assignedUserId", referred_date as "securityReferredDate", referred_by as "securityReferredBy", security_reviewed_date as "securityReviewedDate", security_reviewed_by as "securityReviewedBy"
         from form f where f.prison_id = $1 and f.status = ANY ($2) and f.sequence_no = (select max(f2.sequence_no) from form f2 where f2.booking_id = f.booking_id)`,
       values: ['MDI', ['APPROVED', 'AWAITING_APPROVAL']],
@@ -49,9 +50,9 @@ describe('getCategorisationRecordsByStatus', () => {
 
 describe('getSecurityReviewedCategorisationRecords', () => {
   test('it should generate the correct sql', () => {
-    formClient.getSecurityReviewedCategorisationRecords('MDI')
+    formClient.getSecurityReviewedCategorisationRecords('MDI', mockTransactionalClient)
 
-    expect(db.query).toBeCalledWith({
+    expect(mockTransactionalClient.query).toBeCalledWith({
       text: `select id, booking_id as "bookingId", user_id as "userId", status, form_response as "formObject", assigned_user_id as "assignedUserId", referred_date as "securityReferredDate", referred_by as "securityReferredBy", security_reviewed_date as "securityReviewedDate", security_reviewed_by as "securityReviewedBy"
         from form f where f.prison_id = $1 and security_reviewed_date is not null and f.sequence_no = (select max(f2.sequence_no) from form f2 where f2.booking_id = f.booking_id)`,
       values: ['MDI'],
@@ -61,9 +62,9 @@ describe('getSecurityReviewedCategorisationRecords', () => {
 
 describe('securityReviewed', () => {
   test('it should generate query to update security reviewed columns', () => {
-    formClient.securityReviewed('12345', 'SECURITY', 'Meeeee')
+    formClient.securityReviewed('12345', 'SECURITY', 'Meeeee', mockTransactionalClient)
 
-    expect(db.query).toBeCalledWith({
+    expect(mockTransactionalClient.query).toBeCalledWith({
       text:
         'update form f set security_reviewed_date = CURRENT_TIMESTAMP, security_reviewed_by = $1, status = $2 where booking_id = $3 and f.sequence_no = (select max(f2.sequence_no) from form f2 where f2.booking_id = f.booking_id)',
       values: ['Meeeee', 'SECURITY', '12345'],
@@ -73,9 +74,9 @@ describe('securityReviewed', () => {
 
 describe('create categorisation record', () => {
   test('create categorisation record', () => {
-    formClient.create('bookingId1', 'Meeeee', 'STARTED', 'colleague123', 'MDI', 'A4567RS')
+    formClient.create('bookingId1', 'Meeeee', 'STARTED', 'colleague123', 'MDI', 'A4567RS', mockTransactionalClient)
 
-    expect(db.query).toBeCalledWith({
+    expect(mockTransactionalClient.query).toBeCalledWith({
       text:
         'insert into form (form_response, booking_id, user_id, status, assigned_user_id, sequence_no, prison_id, offender_no, start_date) values ($1, $2, $3, $4, $5, 1, $6, $7, CURRENT_TIMESTAMP)',
       values: [{}, 'bookingId1', 'Meeeee', 'STARTED', 'colleague123', 'MDI', 'A4567RS'],
@@ -85,9 +86,9 @@ describe('create categorisation record', () => {
 
 describe('categorisation record update', () => {
   test('it should update the categorisation record', () => {
-    formClient.update('formId', {}, 'bookingId1', 'STARTED')
+    formClient.update('formId', {}, 'bookingId1', 'STARTED', mockTransactionalClient)
 
-    expect(db.query).toBeCalledWith({
+    expect(mockTransactionalClient.query).toBeCalledWith({
       text:
         'update form f set form_response = $1, status = $2 where f.booking_id = $3 and f.sequence_no = (select max(f2.sequence_no) from form f2 where f2.booking_id = f.booking_id)',
       values: [{}, 'STARTED', 'bookingId1'],
