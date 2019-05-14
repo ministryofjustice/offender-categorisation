583b6d0c05e62516cb34139123a409f98462a531
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2019-05-14 10:39:25 +0100

    limit done tab diplay (approved categorisations) by a configurable APPROVED_DISPLAY_MONTHS env var. Required new approval_date column

---
+++ migrations/20190513000000_add_approval_date_db.js
@@ -0,0 +1,15 @@
+exports.up = knex =>
+  knex.schema
+    .alterTable('form', table => {
+      table.date('approval_date').nullable()
+    })
+    .then(() =>
+      knex.raw(`
+    update form set approval_date = CURRENT_DATE where approval_date is null and status = 'APPROVED'
+      `)
+    )
+
+exports.down = knex =>
+  knex.schema.table('form', table => {
+    table.dropColumn('approval_date')
+  })
