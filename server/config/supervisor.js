bd03c16cce487793fe3a7312148687a74069921a
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2019-06-21 09:57:15 +0100

    cat-266 supervisor approval (recat)

--- server/config/supervisor.js
+++ server/config/supervisor.js
@@ -40,6 +40,47 @@ module.exports = {
     ],
     validate: true,
   },
+  recatReview: {
+    nextPath: {
+      path: '/tasklist/supervisor/outcome/',
+    },
+    fields: [
+      {
+        supervisorCategoryAppropriate: {
+          responseType: 'requiredString',
+          validationMessage: 'Please select yes or no',
+        },
+      },
+      {
+        supervisorOverriddenCategory: {
+          responseType: 'requiredStringIf_supervisorCategoryAppropriate_No',
+          validationMessage: 'Please enter the new category',
+        },
+      },
+      {
+        supervisorOverriddenCategoryText: {
+          responseType: 'requiredStringIf_supervisorCategoryAppropriate_No',
+          validationMessage: 'Please enter the reason why you changed the category',
+        },
+      },
+      {
+        previousOverrideCategoryText: {
+          responseType: 'optionalString',
+        },
+      },
+      {
+        proposedCategory: {
+          responseType: 'optionalString',
+        },
+      },
+      {
+        otherInformationText: {
+          responseType: 'optionalString',
+        },
+      },
+    ],
+    validate: true,
+  },
   confirmBack: {
     fields: [
       {
