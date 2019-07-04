e71f2340d4363482a47021b4abb73e300a9760da
steverendell <steve.rendell@syscon.net>
2019-07-04 16:43:55 +0100

    CAT-337 Manage link landing page

---
+++ integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/specs/LandingPageSpecification.groovy
@@ -0,0 +1,92 @@
+package uk.gov.justice.digital.hmpps.cattool.specs
+
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer
+import geb.spock.GebReportingSpec
+import org.junit.Rule
+import uk.gov.justice.digital.hmpps.cattool.mockapis.Elite2Api
+import uk.gov.justice.digital.hmpps.cattool.mockapis.OauthApi
+import uk.gov.justice.digital.hmpps.cattool.mockapis.RiskProfilerApi
+import uk.gov.justice.digital.hmpps.cattool.model.DatabaseUtils
+import uk.gov.justice.digital.hmpps.cattool.model.TestFixture
+import uk.gov.justice.digital.hmpps.cattool.pages.*
+
+import static uk.gov.justice.digital.hmpps.cattool.model.UserAccount.*
+
+class LandingPageSpecification extends GebReportingSpec {
+
+  def setup() {
+    db.clearDb()
+  }
+
+  @Rule
+  Elite2Api elite2Api = new Elite2Api()
+
+  @Rule
+  RiskProfilerApi riskProfilerApi = new RiskProfilerApi()
+
+  @Rule
+  OauthApi oauthApi = new OauthApi(new WireMockConfiguration()
+    .extensions(new ResponseTemplateTransformer(false)))
+
+  TestFixture fixture = new TestFixture(browser, elite2Api, oauthApi, riskProfilerApi)
+  DatabaseUtils db = new DatabaseUtils()
+
+  def "A recategoriser user can start a recat from the landing page"() {
+
+    given: 'A recategoriser is logged in'
+    elite2Api.stubRecategorise()
+    fixture.loginAs(RECATEGORISER_USER)
+
+    when: 'The user arrives at the landing page'
+    elite2Api.stubGetOffenderDetails(12)
+    elite2Api.stubGetCategory(12, 'C')
+    go '/12'
+
+    then: 'The page contains a recat button'
+    at LandingPage
+    startButton.displayed
+
+    when: 'It is clicked'
+    riskProfilerApi.stubGetSocProfile('B2345YZ', 'C', false)
+    startButton.click()
+
+    then: 'We are sent to the recat tasklist'
+    at TasklistRecatPage
+    currentUrl.contains '/tasklistRecat/12'
+  }
+
+  def "A recategoriser user sees a warning for initial cat"() {
+
+    given: 'A recategoriser is logged in'
+    elite2Api.stubRecategorise()
+    fixture.loginAs(RECATEGORISER_USER)
+
+    when: 'The user arrives at the landing page'
+    elite2Api.stubGetOffenderDetails(12, 'B2345YZ', false,  false,  'U')
+    elite2Api.stubGetCategory(12, 'U')
+    go '/12'
+
+    then: 'The page contains an initial cat warning'
+    at LandingPage
+    !startButton.displayed
+    warning.text() contains 'This prisoner seems to need an INITIAL category'
+  }
+
+  def "A recategoriser user sees a warning for cat A"() {
+
+    given: 'A recategoriser is logged in'
+    elite2Api.stubRecategorise()
+    fixture.loginAs(RECATEGORISER_USER)
+
+    when: 'The user arrives at the landing page'
+    elite2Api.stubGetOffenderDetails(12,'B2345YZ', false,  false,  'A')
+    elite2Api.stubGetCategory(12, 'A')
+    go '/12'
+
+    then: 'The page contains a warning'
+    at LandingPage
+    !startButton.displayed
+    warning.text() contains 'This prisoner is Cat A. They cannot be categorised here.'
+  }
+}
