dc2fe9432ef7667181f5195aecfd4e64fe205a50
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2019-07-10 08:58:23 +0100

    DT-72: Removed custodyApi ping call from Dependency Health check

--- server/app.js
+++ server/app.js
@@ -141,12 +141,7 @@ module.exports = function createApp({
   })
   app.use('/favicon.ico', express.static(path.join(__dirname, '../assets/images/favicon.ico'), cacheControl))
 
-  const health = healthFactory(
-    config.apis.oauth2.url,
-    config.apis.elite2.url,
-    config.apis.custody.url,
-    config.apis.riskProfiler.url
-  )
+  const health = healthFactory(config.apis.oauth2.url, config.apis.elite2.url, config.apis.riskProfiler.url)
   app.get('/health', (req, res, next) => {
     health((err, result) => {
       if (err) {
