17ad4eab5d74cdb49b0fe8a6d77b1cb7c6d91f38
steverendell <steve.rendell@syscon.net>
2019-07-31 09:12:38 +0100

    CAT-432 Improve format for printing - build error

--- bin/build-css.bat
+++ bin/build-css.bat
@@ -1,4 +1,4 @@
 
-node node_modules\node-sass\bin\node-sass --include-path node_modules\govuk-frontend assets/sass/application.sass assets/stylesheets/application.css
-node node_modules\node-sass\bin\node-sass --include-path node_modules\govuk-frontend assets/sass/application-ie8.sass assets/stylesheets/application-ie8.css
+node node_modules\node-sass\bin\node-sass --include-path node_modules\govuk-frontend --include-path node_modules/@ministryofjustice assets/sass/application.sass assets/stylesheets/application.css
+node node_modules\node-sass\bin\node-sass --include-path node_modules\govuk-frontend --include-path node_modules/@ministryofjustice assets/sass/application-ie8.sass assets/stylesheets/application-ie8.css
 rem --output-style  nested
