4fb5d6bcf7da228cdf79b22e0f996f8d2047c06d
steverendell <steve.rendell@syscon.net>
2019-08-06 15:11:19 +0100

    CAT-442 and CAT-451 initial commit for sorts in wrong order

--- server/data/formClient.js
+++ server/data/formClient.js
@@ -52,7 +52,7 @@ module.exports = {
   },
 
   getSecurityReviewedCategorisationRecords(agencyId, transactionalClient) {
-    logger.debug(`getSecurityReviewedOffenders called for ${agencyId}`)
+    logger.debug(`getSecurityReviewedCategorisationRecords called for ${agencyId}`)
     const query = {
       text: `select id, booking_id as "bookingId", user_id as "userId", status, form_response as "formObject", assigned_user_id as "assignedUserId", referred_date as "securityReferredDate", referred_by as "securityReferredBy", security_reviewed_date as "securityReviewedDate", security_reviewed_by as "securityReviewedBy", approval_date as "approvalDate", offender_no as "offenderNo", cat_type as "catType"
         from form f where f.prison_id = $1 and security_reviewed_date is not null ${sequenceClause}`,
