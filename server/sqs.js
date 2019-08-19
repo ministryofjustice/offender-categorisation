2e531eb6081bf95bcc38aea92c8f16b6edd98808
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2019-08-19 16:11:26 +0100

    wip

---
+++ server/sqs.js
@@ -0,0 +1,23 @@
+const { Consumer } = require('sqs-consumer')
+const logger = require('../log.js')
+const config = require('./config')
+
+const app = Consumer.create({
+  queueUrl: config.sqs.riskProfilerQueue,
+  handleMessage: async message => {
+    logger.info(`received ${message} payload`)
+    const change = JSON.parse(message.Body)
+    logger.info(`Old profile: ${change.oldProfile}`)
+    logger.info(`New profile: ${change.newProfile}`)
+  },
+})
+
+app.on('error', err => {
+  logger.error(err.message)
+})
+
+app.on('processing_error', err => {
+  logger.error(err.message)
+})
+
+app.start()
