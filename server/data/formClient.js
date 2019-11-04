f2d2fe5340d97515480b314166a31ccde08ac4c1
steverendell <steve.rendell@syscon.net>
2019-11-04 09:44:47 +0000

    CAT-485 Allow users to cancel a submission as long as it wasn't approved - PR comments

--- server/data/formClient.js
+++ server/data/formClient.js
@@ -33,6 +33,7 @@ module.exports = {
   },
 
   getFormDataUsingSequence(bookingId, sequenceNo, transactionalClient) {
+    logger.debug(`getFormDataUsingSequence called for ${bookingId}, sequenceNo ${sequenceNo}`)
     const query = {
       text: `${selectClause} from form f where f.booking_id = $1 and f.sequence_no = $2`,
       values: [bookingId, sequenceNo],
@@ -41,6 +42,7 @@ module.exports = {
   },
 
   getHistoricalFormData(bookingId, transactionalClient) {
+    logger.debug(`getHistoricalFormData called for ${bookingId}`)
     const query = {
       text: `select booking_id    as "bookingId",
                     offender_no   as "offenderNo",
@@ -105,16 +107,16 @@ module.exports = {
   },
 
   mergeRiskChangeForOffender(offenderNo, newProfile, transactionalClient) {
-    logger.debug(`mergeRiskChangeForOffender called with offenderNo ${offenderNo}`)
+    logger.info(`mergeRiskChangeForOffender called with offenderNo ${offenderNo}`)
     const query = {
-      text: `update risk_change set new_profile =$2, raised_date = CURRENT_TIMESTAMP where r.offender_no= $1 and r.status = 'NEW'`,
+      text: `update risk_change set new_profile = $2, raised_date = CURRENT_TIMESTAMP where r.offender_no= $1 and r.status = 'NEW'`,
       values: [offenderNo, newProfile],
     }
     return transactionalClient.query(query)
   },
 
   referToSecurity(bookingId, userId, status, transactionalClient) {
-    logger.debug(`referToSecurity called for ${userId}, status ${status} and booking id ${bookingId}`)
+    logger.info(`referToSecurity called for ${userId}, status ${status} and booking id ${bookingId}`)
     const query = {
       text: `update form f set status = $1, referred_date = CURRENT_TIMESTAMP, referred_by = $2 where f.booking_id = $3 ${sequenceClause}`,
       values: [status, userId, bookingId],
@@ -123,7 +125,7 @@ module.exports = {
   },
 
   securityReviewed(bookingId, status, userId, transactionalClient) {
-    logger.debug(`securityReviewed called for ${userId} with status ${status} and booking id ${bookingId}`)
+    logger.info(`securityReviewed called for ${userId} with status ${status} and booking id ${bookingId}`)
     const query = {
       text: `update form f set security_reviewed_date = CURRENT_TIMESTAMP, security_reviewed_by = $1, status = $2 where f.booking_id = $3 ${sequenceClause}`,
       values: [userId, status, bookingId],
@@ -132,7 +134,7 @@ module.exports = {
   },
 
   updateStatus(bookingId, status, transactionalClient) {
-    logger.debug(`updateStatus called for booking id ${bookingId} and status ${status}`)
+    logger.info(`updateStatus called for booking id ${bookingId} and status ${status}`)
     const query = {
       text: `update form f set status = $1 where f.booking_id = $2 ${sequenceClause}`,
       values: [status, bookingId],
@@ -141,7 +143,7 @@ module.exports = {
   },
 
   updateRecordWithNomisSeqNumber(bookingId, seq, transactionalClient) {
-    logger.debug(`updateRecordWithNomisSeqNumber called for booking id ${bookingId} and seq ${seq}`)
+    logger.info(`updateRecordWithNomisSeqNumber called for booking id ${bookingId} and seq ${seq}`)
     const query = {
       text: `update form f set nomis_sequence_no = $1 where f.booking_id = $2 ${sequenceClause}`,
       values: [seq, bookingId],
@@ -150,7 +152,7 @@ module.exports = {
   },
 
   updateFormData(bookingId, formResponse, transactionalClient) {
-    logger.debug(`updateFormData for booking id ${bookingId}`)
+    logger.info(`updateFormData for booking id ${bookingId}`)
     const query = {
       text: `update form f set form_response = $1 where f.booking_id = $2 ${sequenceClause}`,
       values: [formResponse, bookingId],
@@ -159,7 +161,7 @@ module.exports = {
   },
 
   updateRiskProfileData(bookingId, data, transactionalClient) {
-    logger.debug(`mergeRiskProfileData for booking id ${bookingId}`)
+    logger.info(`updateRiskProfileData called for booking id ${bookingId}`)
     const query = {
       text: `update form f set risk_profile = $1 where f.booking_id = $2 ${sequenceClause}`,
       values: [data, bookingId],
@@ -168,7 +170,7 @@ module.exports = {
   },
 
   cancel(bookingId, user, transactionalClient) {
-    logger.debug(`cancel called for booking id ${bookingId} and user ${user}`)
+    logger.info(`cancel called for booking id ${bookingId} and user ${user}`)
     const query = {
       text: `update form f set status = $1, cancelled_by = $2, cancelled_date = CURRENT_TIMESTAMP where f.booking_id = $3 ${sequenceClause}`,
       values: [Status.CANCELLED.name, user, bookingId],
@@ -177,7 +179,7 @@ module.exports = {
   },
 
   supervisorApproval(formResponse, bookingId, userId, transactionalClient) {
-    logger.debug(`recording supervisor approval for booking id ${bookingId}`)
+    logger.info(`recording supervisor approval for booking id ${bookingId} and user ${userId}`)
     const query = {
       text: `update form f set form_response = $1, status = $2, approved_by = $3, approval_date = CURRENT_DATE where f.booking_id = $4 ${sequenceClause}`,
       values: [formResponse, 'APPROVED', userId, bookingId],
@@ -186,7 +188,7 @@ module.exports = {
   },
 
   categoriserDecisionWithFormResponse(formResponse, bookingId, userId, transactionalClient) {
-    logger.debug(`recording assessment decision (awaiting approval) for booking id ${bookingId}`)
+    logger.info(`recording assessment decision (awaiting approval) for booking id ${bookingId} and user ${userId}`)
     const query = {
       text: `update form f set form_response = $1, status = $2, assessed_by = $3, assessment_date = CURRENT_DATE where f.booking_id = $4 ${sequenceClause}`,
       values: [formResponse, 'AWAITING_APPROVAL', userId, bookingId],
@@ -195,7 +197,7 @@ module.exports = {
   },
 
   categoriserDecision(bookingId, userId, transactionalClient) {
-    logger.debug(`recording assessment decision (awaiting approval) for booking id ${bookingId}`)
+    logger.info(`recording assessment decision (awaiting approval) for booking id ${bookingId} and user ${userId}`)
     const query = {
       text: `update form f set status = $1, assessed_by = $2, assessment_date = CURRENT_DATE where f.booking_id = $3 ${sequenceClause}`,
       values: ['AWAITING_APPROVAL', userId, bookingId],
@@ -204,7 +206,7 @@ module.exports = {
   },
 
   update(formResponse, bookingId, status, transactionalClient) {
-    logger.debug(`updating record for booking id ${bookingId}`)
+    logger.info(`updating record for booking id ${bookingId}`)
     const query = {
       text: `update form f set form_response = $1, status = $2 where f.booking_id = $3 ${sequenceClause}`,
       values: [formResponse, status, bookingId],
@@ -213,7 +215,7 @@ module.exports = {
   },
 
   createRiskChange({ agencyId, offenderNo, oldProfile, newProfile, transactionalClient }) {
-    logger.debug(`creating risk_change record for offender no  ${offenderNo}`)
+    logger.info(`creating risk_change record for offender no ${offenderNo}`)
     const query = {
       text: `insert into risk_change ( prison_id, offender_no, old_profile, new_profile, raised_date ) values ($1, $2, $3, $4, CURRENT_TIMESTAMP )`,
       values: [agencyId, offenderNo, oldProfile, newProfile],
@@ -222,7 +224,7 @@ module.exports = {
   },
 
   updateNewRiskChangeStatus({ offenderNo, userId, status, transactionalClient }) {
-    logger.debug(`updating risk_change status for offender no  ${offenderNo} with status ${status}`)
+    logger.info(`updating risk_change status for offender no  ${offenderNo} with status ${status}`)
     const query = {
       text: `update risk_change set status = $1, user_id = $2 where offender_no = $3 and status = 'NEW'`,
       values: [status, userId, offenderNo],
@@ -231,7 +233,7 @@ module.exports = {
   },
 
   createSecurityReferral({ agencyId, offenderNo, userId, transactionalClient }) {
-    logger.debug(`creating security_referral record for ${offenderNo}`)
+    logger.info(`creating security_referral record for ${offenderNo}`)
     const query = {
       text: `insert into security_referral ( prison_id, offender_no, user_id, raised_date )
         values ($1, $2, $3, CURRENT_TIMESTAMP )
@@ -256,7 +258,7 @@ module.exports = {
   },
 
   setSecurityReferralProcessed(offenderNo, transactionalClient) {
-    logger.debug(`setSecurityReferralProcessed for ${offenderNo}`)
+    logger.info(`setSecurityReferralProcessed for ${offenderNo}`)
     const query = {
       text: `update security_referral set status='REFERRED', processed_date=CURRENT_TIMESTAMP where offender_no=$1`,
       values: [offenderNo],
@@ -276,7 +278,7 @@ module.exports = {
     dueByDate,
     transactionalClient,
   }) {
-    logger.debug(`creating categorisation record for booking id ${bookingId}`)
+    logger.info(`creating categorisation record for booking id ${bookingId}, offenderNo ${offenderNo}`)
     const query = {
       text: `insert into form (
               form_response, booking_id, user_id, status, assigned_user_id, sequence_no, prison_id, offender_no, start_date, cat_type, review_reason, due_by_date
