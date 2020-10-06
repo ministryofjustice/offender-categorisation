78ab46980911225e2c3879dfdf242664fee8ba0b
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2020-10-06 09:22:23 +0100

    CAT-650 Content Changes to the Intial cat process (#438)
    Change to violence warning throughout

--- integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/mockapis/RiskProfilerApi.groovy
+++ integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/mockapis/RiskProfilerApi.groovy
@@ -15,99 +15,100 @@ class RiskProfilerApi extends WireMockRule {
     this.stubFor(
       get("/risk-profile/soc/${offenderno}")
         .willReturn(
-        aResponse()
-          .withStatus(200)
-          .withHeader('Content-Type', 'application/json')
-          .withBody(JsonOutput.toJson([
-          nomsId                   : offenderno,
-          riskType                 : 'SOC',
-          provisionalCategorisation: category,
-          transferToSecurity       : transferToSecurity
-        ]))))
+          aResponse()
+            .withStatus(200)
+            .withHeader('Content-Type', 'application/json')
+            .withBody(JsonOutput.toJson([
+              nomsId                   : offenderno,
+              riskType                 : 'SOC',
+              provisionalCategorisation: category,
+              transferToSecurity       : transferToSecurity
+            ]))))
   }
 
   void stubGetViolenceProfile(String offenderno, String category, boolean veryHighRiskViolentOffender, boolean notifySafetyCustodyLead, boolean displayAssaults) {
     this.stubFor(
       get("/risk-profile/violence/${offenderno}")
         .willReturn(
-        aResponse()
-          .withStatus(200)
-          .withHeader('Content-Type', 'application/json')
-          .withBody(JsonOutput.toJson([
-          nomsId                     : offenderno,
-          riskType                   : 'VIOLENCE',
-          provisionalCategorisation  : category,
-          veryHighRiskViolentOffender: veryHighRiskViolentOffender,
-          notifySafetyCustodyLead    : notifySafetyCustodyLead,
-          displayAssaults            : displayAssaults,
-          numberOfAssaults           : 5,
-          numberOfSeriousAssaults    : 2
-        ]))))
+          aResponse()
+            .withStatus(200)
+            .withHeader('Content-Type', 'application/json')
+            .withBody(JsonOutput.toJson([
+              nomsId                     : offenderno,
+              riskType                   : 'VIOLENCE',
+              provisionalCategorisation  : category,
+              veryHighRiskViolentOffender: veryHighRiskViolentOffender,
+              notifySafetyCustodyLead    : notifySafetyCustodyLead,
+              displayAssaults            : displayAssaults,
+              numberOfAssaults           : 5,
+              numberOfSeriousAssaults    : 2,
+              numberOfNonSeriousAssaults : 3
+            ]))))
   }
 
   void stubGetEscapeProfile(String offenderno, String category, boolean onEscapeList, boolean activeOnEscapeList) {
     this.stubFor(
       get("/risk-profile/escape/${offenderno}")
         .willReturn(
-        aResponse()
-          .withStatus(200)
-          .withHeader('Content-Type', 'application/json')
-          .withBody(JsonOutput.toJson([
-          nomsId                   : offenderno,
-          riskType                 : 'ESCAPE',
-          provisionalCategorisation: category,
-          activeEscapeList         : onEscapeList,
-          activeEscapeRisk         : activeOnEscapeList,
-          escapeListAlerts: [
-            [
-              alertCode: "XEL",
-              alertCodeDescription: "Escape List",
-              comment: "First xel comment",
-              dateCreated: "2016-09-14",
-              expired: false,
-              active: true
-            ],
-            [
-              alertCode: "XEL",
-              alertCodeDescription: "Escape List",
-              comment: '''
+          aResponse()
+            .withStatus(200)
+            .withHeader('Content-Type', 'application/json')
+            .withBody(JsonOutput.toJson([
+              nomsId                   : offenderno,
+              riskType                 : 'ESCAPE',
+              provisionalCategorisation: category,
+              activeEscapeList         : onEscapeList,
+              activeEscapeRisk         : activeOnEscapeList,
+              escapeListAlerts         : [
+                [
+                  alertCode           : "XEL",
+                  alertCodeDescription: "Escape List",
+                  comment             : "First xel comment",
+                  dateCreated         : "2016-09-14",
+                  expired             : false,
+                  active              : true
+                ],
+                [
+                  alertCode           : "XEL",
+                  alertCodeDescription: "Escape List",
+                  comment             : '''
 Second xel comment with lengthy text comment with lengthy text comment with lengthy text comment with lengthy text
  comment with lengthy text comment with lengthy text comment with lengthy text
   comment with lengthy text comment with lengthy text comment with lengthy text
    comment with lengthy text comment with lengthy text comment with lengthy text
 ''',
-              dateCreated: "2016-09-15",
-              expired: true,
-              active: false
-            ]
-          ],
-          escapeRiskAlerts: [
-            [
-              alertCode: "XER",
-              alertCodeDescription: "Escape Risk",
-              comment: "First xer comment",
-              dateCreated: "2016-09-16",
-              expired: false,
-              active: true
-            ],
-          ]
-        ]))))
+                  dateCreated         : "2016-09-15",
+                  expired             : true,
+                  active              : false
+                ]
+              ],
+              escapeRiskAlerts         : [
+                [
+                  alertCode           : "XER",
+                  alertCodeDescription: "Escape Risk",
+                  comment             : "First xer comment",
+                  dateCreated         : "2016-09-16",
+                  expired             : false,
+                  active              : true
+                ],
+              ]
+            ]))))
   }
 
   void stubGetExtremismProfile(String offenderno, String category, boolean increasedRisk, boolean notifyRegionalCTLead, boolean previousOffences = false) {
     this.stubFor(
       get("/risk-profile/extremism/$offenderno?previousOffences=$previousOffences")
         .willReturn(
-        aResponse()
-          .withStatus(200)
-          .withHeader('Content-Type', 'application/json')
-          .withBody(JsonOutput.toJson([
-          nomsId                   : offenderno,
-          riskType                 : 'EXTREMISM',
-          provisionalCategorisation: category,
-          increasedRiskOfExtremism : increasedRisk,
-          notifyRegionalCTLead     : notifyRegionalCTLead
-        ]))))
+          aResponse()
+            .withStatus(200)
+            .withHeader('Content-Type', 'application/json')
+            .withBody(JsonOutput.toJson([
+              nomsId                   : offenderno,
+              riskType                 : 'EXTREMISM',
+              provisionalCategorisation: category,
+              increasedRiskOfExtremism : increasedRisk,
+              notifyRegionalCTLead     : notifyRegionalCTLead
+            ]))))
   }
 
   void stubGetLifeProfile(String offenderno, String category) {
