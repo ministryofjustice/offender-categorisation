1e3e0f133ef7632872c5bb2fb7d21f8dc0ca56f6
Connor Glynn <connor.glynn@digital.justice.gov.uk>
2021-01-28 14:54:12 +0000

    CAT-686: fix int test

--- integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/pages/LiteCategoriesAlreadyApprovedPage.groovy
+++ integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/pages/LiteCategoriesAlreadyApprovedPage.groovy
@@ -3,11 +3,11 @@ package uk.gov.justice.digital.hmpps.cattool.pages
 class LiteCategoriesAlreadyApprovedPage extends HeaderPage {
 
   static at = {
-    panelText.text() == 'Categorisation has already been approved'
+    warningText.text().contains('Categorisation has already been approved')
   }
 
   static content = {
-    panelText { $('h1.govuk-panel__title') }
+    warningText { $('.govuk-warning-text') }
     finishButton { $('a.govuk-button') }
   }
 }
