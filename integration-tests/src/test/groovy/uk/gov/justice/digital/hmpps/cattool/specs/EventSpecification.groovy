27225c04464d50997b310c5ca7cdf4e2f5655c64
steverendell <steve.rendell@syscon.net>
2021-04-21 13:16:09 +0100

    CAT-687 Merge event not updating all tables - QA fix

--- integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/specs/EventSpecification.groovy
+++ integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/specs/EventSpecification.groovy
@@ -80,8 +80,7 @@ class EventSpecification extends GebReportingSpec {
       def data = db.getData(123)
       data.id == [1, 2]
       data.prison_id == ['MDI', 'LEI']
-    }
-    waitFor {
+
       db.getData(124).prison_id == ['MDI']
 
       def lite = db.getLiteData(123)
@@ -92,8 +91,76 @@ class EventSpecification extends GebReportingSpec {
       def rc = db.getRiskChange('A1234AA')
       rc.id == [1, 2]
       rc.prison_id == ['MDI', 'LEI']
+
       db.getSecurityData('A1234AA').prison_id == ['LEI']
       db.getSecurityData('A1234AB').prison_id == ['MDI']
+    }
+  }
+
+  def "merge events should change the offenderNo in all tables"() {
+
+    def bookingId = 123
+
+    db.doCreateCompleteRow(1, bookingId, '{}', 'CATEGORISER_USER', 'APPROVED', 'INITIAL', null, null, null,
+      1, null, 'MDI', 'A1234AA', 'current_timestamp(2)', null, null)
+    db.doCreateCompleteRow(2, bookingId, '{}', 'CATEGORISER_USER', 'STARTED', 'INITIAL', null, null, null,
+      2, null, 'MDI', 'A1234AA', 'current_timestamp(2)', null, null)
+    db.doCreateCompleteRow(3, 124, '{}', 'CATEGORISER_USER', 'STARTED', 'INITIAL', null, null, null,
+      1, null, 'MDI', 'A1234AA', 'current_timestamp(2)', null, null)
+    db.doCreateCompleteRow(4, 125, '{}', 'CATEGORISER_USER', 'STARTED', 'INITIAL', null, null, null,
+      1, null, 'MDI', 'Z9999ZZ', 'current_timestamp(2)', null, null)
+
+    db.createLiteCategorisation(bookingId, 1, 'A1234AA', 'V', 'MDI')
+    db.createUnapprovedLiteCategorisation(bookingId, 2, 'A1234AA', 'V', 'MDI', 'CATEGORISER_USER')
+    db.createLiteCategorisation(124, 1, 'A1234AA', 'V', 'MDI')
+
+    db.createRiskChange(1, 'A1234AA', null, 'PROCESSED', '{}', '{}', 'MDI', LocalDate.now())
+    db.createRiskChange(2, 'A1234AA', null, 'NEW', '{}', '{}', 'MDI', LocalDate.now())
+
+    db.createSecurityData('A1234AB', 'MDI', 1, 'COMPLETED')
+    db.createSecurityData('A1234AA', 'MDI', 2, 'NEW')
+
+    elite2Api.stubGetBasicOffenderDetails(bookingId, 'A1234AB')
+    elite2Api.stubGetIdentifiersByBookingId(bookingId)
+
+    when: 'a merge event arrives merging A1234AA to A1234AB'
+
+    sqs.sendMessage('http://localhost:4576/queue/event', """{
+      "Message" : "{ \\"eventType\\": \\"BOOKING_NUMBER-CHANGED\\", \\"bookingId\\": ${bookingId}, \\"offenderId\\":1577871, \\"previousBookingNumber\\": \\"M07037\\",\\"eventDatetime\\": \\"2020-02-25T16:00:00.0\\", \\"nomisEventType\\": \\"BOOK_UPD_OASYS\\" }",
+      "MessageAttributes" : {
+        "eventType" : { "Type" : "String", "Value" : "BOOKING_NUMBER-CHANGED" } ,
+        "id" : { "Type" : "String", "Value" : "f9f1e5e4-999a-78ad-d1d8-442d8864481a" } ,
+        "contentType" : { "Type" : "String", "Value" : "text/plain;charset=UTF-8" } ,
+        "timestamp" : { "Type" : "Number.java.lang.Long", "Value" : "1579014873619" }
+      }
+    }""")
+
+    /*
+    """
+{ "Message" : "{\\"eventType\\":\\"BOOKING_NUMBER-CHANGED\\",\\"eventDatetime\\":\\"2021-04-19T15:53:26.870351\\",\\"offenderId\\":1577871,\\"bookingId\\":2682588,\\"previousBookingNumber\\":\\"M07037\\",\\"nomisEventType\\":\\"BOOK_UPD_OASYS\\"}",
+ "Timestamp" : "2021-04-19T15:00:45.767Z", "SignatureVersion" : "1",
+ "Signature" : "SN7KVbzfOREy89YzcyIjs3GRUINuw06ZHohg/IeK8wAqI5IdnN//c8k7NmDT0JDKQxM2a6NReeTt+JuUlC7dflW3Q5wRbZlan9JXs8+o8ahRfZPqBaexoMcXl1UF6qs/GWbPDSdyIQhNGfCSctr7CGWUM2H0znoupJndDtk/MBpnu7BR8sk4pJu4C1+fvi8o1WCe9H7hpJq5YqOFu1y1KhzBbfrXHG+gB3ACX1WSrEdsqasXc6Hp7Qj3taZQyHjPuWm10UHCl5MKldkHKRx5CBUMWicNlXrXSgUdkLyi1+VfyWIm6zLs6TkN1Zxm6trTh327GFS+PbKigLyGm9Hf2w==",
+ "SigningCertURL" : "https://sns.eu-west-2.amazonaws.com/SimpleNotificationService-010a507c1833636cd94bdb98bd93083a.pem",
+ "UnsubscribeURL" : "https://sns.eu-west-2.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:eu-west-2:754256621582:cloud-platform-Digital-Prison-Services-160f3055cc4e04c4105ee85f2ed1fccb:f07f7afc-eee4-4859-81d6-b836efab36ca",
+   "MessageAttributes" : { "eventType" : {"Type":"String","Value":"BOOKING_NUMBER-CHANGED"}, "id" : {"Type":"String","Value":"f23d0817-095d-a191-19ce-5ec6c3a7c220"},
+      "contentType" : {"Type":"String","Value":"text/plain;charset=UTF-8"}, "timestamp" : {"Type":"Number.java.lang.Long","Value":"1618844445759"} } }
+"""*/
+
+    then: 'The offenderNo is updated as follows'
+
+    waitFor {
+      db.getData(123).offender_no == ['A1234AB', 'A1234AB']
+      db.getData(124).offender_no == ['A1234AB']
+      db.getData(125).offender_no == ['Z9999ZZ']
+
+      db.getLiteData(123).offender_no == ['A1234AB', 'A1234AB']
+      db.getLiteData(124).offender_no == ['A1234AB']
+
+      db.getRiskChange('A1234AA').id == []
+      db.getRiskChange('A1234AB').id == [1,2]
+
+      db.getSecurityData('A1234AA').id == [2]
+      db.getSecurityData('A1234AB').id == []
     }
   }
 }
