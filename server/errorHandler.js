c25301c909ed3bec5198f67abdab2b494d994bd7
thirlz95 <johnthirlaway95@gmail.com>
2022-06-22 11:39:34 +0100

    DSE-310: added prettier fixes

--- server/errorHandler.js
+++ server/errorHandler.js
@@ -3,17 +3,17 @@ const logger = require('../log')
 
 module.exports =
   production =>
-    // NOTE 'next' param MUST be included so that express recognises this as an error handler
-    // eslint-disable-next-line no-unused-vars
-    (error, req, res, next) => {
-      logger.error(error)
+  // NOTE 'next' param MUST be included so that express recognises this as an error handler
+  // eslint-disable-next-line no-unused-vars
+  (error, req, res, next) => {
+    logger.error(error)
 
-      // code to handle unknown errors
-      const prodMessage = `Something went wrong at ${moment()}. The error has been logged. Please try again`
-      const status = error.status || 500
-      res.locals.message = production ? prodMessage : error.message
-      res.locals.status = error.status
-      res.locals.stack = production ? null : error.stack
-      res.status(error.status || 500)
-      return res.render(status >= 500 ? 'pages/serviceUnavaliable' : 'pages/error')
-    }
+    // code to handle unknown errors
+    const prodMessage = `Something went wrong at ${moment()}. The error has been logged. Please try again`
+    const status = error.status || 500
+    res.locals.message = production ? prodMessage : error.message
+    res.locals.status = error.status
+    res.locals.stack = production ? null : error.stack
+    res.status(error.status || 500)
+    return res.render(status >= 500 ? 'pages/serviceUnavaliable' : 'pages/error')
+  }
