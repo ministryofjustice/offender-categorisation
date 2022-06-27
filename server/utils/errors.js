4374d57e6cf18471fdde5dc8a90989be5a7ff4e5
thirlz95 <johnthirlaway95@gmail.com>
2022-06-27 16:16:13 +0100

    Update errors.js

--- server/utils/errors.js
+++ server/utils/errors.js
@@ -4,7 +4,6 @@ module.exports = {
 
 function unauthorisedError(message) {
   const error = new Error(message || 'Unauthorised access: required role not present')
-  console.log("Errors.js", error)
   error.status = 403
   return error
 }
