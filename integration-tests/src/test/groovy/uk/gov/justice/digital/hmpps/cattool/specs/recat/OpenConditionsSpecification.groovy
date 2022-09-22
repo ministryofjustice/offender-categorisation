afdb288a9d19415df896ae42201b43070e71ff76
Olusola-Opencast <olusola.fadero@digital.justice.gov.uk>
2022-09-22 10:38:42 +0100

    CAT-1132 flakey test fix

--- integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/specs/recat/OpenConditionsSpecification.groovy
+++ integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/specs/recat/OpenConditionsSpecification.groovy
@@ -56,8 +56,7 @@ class OpenConditionsSpecification extends AbstractSpecification {
 
     when: 'I submit the page'
     threeOrMoreYearsYes.click()
-
-    justifyYes.check()
+    wait(10)
     justifyYes.click()
     justifyText << 'details text'
     submitButton.click()
