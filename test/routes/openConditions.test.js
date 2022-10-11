c0c2f6d23893e7b92ce52b28d80bed36d7934bc3
Olusola-Opencast <olusola.fadero@digital.justice.gov.uk>
2022-10-11 16:11:56 +0100

    added validation test for Previous Sentences page

--- test/routes/openConditions.test.js
+++ test/routes/openConditions.test.js
@@ -16,6 +16,7 @@ let roles
 jest.doMock('jwt-decode', () => jest.fn(() => ({ authorities: roles })))
 
 const createRouter = require('../../server/routes/openConditions')
+const fieldValidation = require('../../server/utils/fieldValidation')
 
 const formConfig = {
   ratings,
@@ -278,5 +279,35 @@ describe('open conditions', () => {
       .send({ stillRefer: 'No', catType: 'RECAT' })
       .expect(302)
       .expect('Location', `/tasklistRecat/12345`)
+  })
+})
+
+describe('Validating Previous Sentences page', () => {
+  it('Validation should return the correct error message for blank response to "Do they have a previous sentence of 7 years or more?"', () => {
+    const formResponse = { sevenOrMoreYears: '', releasedLastFiveYears: '' }
+    expect(fieldValidation.validate(formResponse, openConditions.previousSentences)).toEqual([
+      { href: '#sevenOrMoreYears', text: 'Select yes if they have a previous sentence of 7 years or more' },
+    ])
+  })
+  it('Validation should return the correct error message for blank response to "Were they released from this sentence in the last 5 years?"', () => {
+    const formResponse = { sevenOrMoreYears: 'Yes', releasedLastFiveYears: '' }
+    expect(fieldValidation.validate(formResponse, openConditions.previousSentences)).toEqual([
+      {
+        href: '#releasedLastFiveYears',
+        text: 'Select yes if they were released from this sentence in the last 5 years',
+      },
+    ])
+  })
+  it('Validation should return no error messages with "No" to "Do they have a previous sentence of 7 years or more?"', () => {
+    const formResponse = { sevenOrMoreYears: 'No', releasedLastFiveYears: '' }
+    expect(fieldValidation.validate(formResponse, openConditions.previousSentences)).toEqual([])
+  })
+  it('Validation should return no error messages for Previous Sentences page with answers "Yes" and "No"', () => {
+    const formResponse = { sevenOrMoreYears: 'Yes', releasedLastFiveYears: 'No' }
+    expect(fieldValidation.validate(formResponse, openConditions.previousSentences)).toEqual([])
+  })
+  it('Validation should return no error messages for Previous Sentences page with answers "Yes" and "Yes"', () => {
+    const formResponse = { sevenOrMoreYears: 'Yes', releasedLastFiveYears: 'Yes' }
+    expect(fieldValidation.validate(formResponse, openConditions.previousSentences)).toEqual([])
   })
 })
