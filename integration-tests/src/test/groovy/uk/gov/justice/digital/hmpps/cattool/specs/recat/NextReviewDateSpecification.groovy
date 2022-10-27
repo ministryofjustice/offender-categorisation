18c048eb6a459d445fc63d7cb8b7e7a404d1dabd
kkonuganti <kartheek.konuganti@digital.justice.gov.uk>
2022-10-27 13:15:46 +0100

    cat-1133 amend error message for text box (#583)

--- integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/specs/recat/NextReviewDateSpecification.groovy
+++ integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/specs/recat/NextReviewDateSpecification.groovy
@@ -124,8 +124,8 @@ class NextReviewDateSpecification extends AbstractSpecification {
     submitButton.click()
 
     then: 'there are 2 validation errors'
-    errorSummaries*.text() == ['The review date must be a real date', 'Please enter a reason for the change']
-    errors*.text() == ['Error:\nThe review date must be a real date', 'Error:\nPlease enter details']
+    errorSummaries*.text() == ['The review date must be a real date', 'Enter reason for date change']
+    errors*.text() == ['Error:\nThe review date must be a real date', 'Error:\nEnter reason for date change']
 
     when: 'reason entered'
     reason = 'A test reason'
