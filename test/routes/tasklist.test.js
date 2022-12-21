527b6d4dd9e85fea1dfec775603c3ae836cc6812
Olusola-Opencast <olusola.fadero@digital.justice.gov.uk>
2022-12-21 11:51:24 +0000

    removed bracket

--- test/routes/tasklist.test.js
+++ test/routes/tasklist.test.js
@@ -205,13 +205,12 @@ describe('GET /tasklist/', () => {
       })
   })
 
-  test('should render categoriserSubmitted page', () => {
+  test('should render categoriserSubmitted page', () =>
     request(app)
       .get('/categoriserSubmitted/12345')
       .expect(200)
       .expect('Content-Type', /html/)
       .expect(res => {
         expect(res.text).toContain('Submitted for approval')
-      })
-  })
+      }))
 })
