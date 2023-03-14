5e9043c78e2f24f721b9a52906348d635b7c59d3
nnehabharti <69511231+nnehabharti@users.noreply.github.com>
2023-03-14 15:20:45 +0000

    Cat 1496 women yoi recat it (#675)
    * CAT-1496-add women recat journey integration tests
    
    ---------
    
    Co-authored-by: Olusola-Opencast <olusola.fadero@digital.justice.gov.uk>

--- integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/specs/DashboardWomenSpecification.groovy
+++ integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/specs/DashboardWomenSpecification.groovy
@@ -68,7 +68,7 @@ class DashboardWomenSpecification extends AbstractSpecification {
     return b
   }
 
-  def "The initial cat dashboard should show correct women stats data"() {
+  def "The initial cat dashboard should show correct women and YOI stats data"() {
     // ignored as AWAITING_APPROVAL or wrong cat type
     db.doCreateCompleteRow(-10, 700, ic('R').build(), 'FEMALE_USER', 'AWAITING_APPROVAL', 'INITIAL', null, null, null, 1, '{}',
       'PFI', "ON700", "'2019-07-01Z00:00'", null, null, '2019-07-29', '2019-07-22', '2019-08-03')
@@ -95,6 +95,15 @@ class DashboardWomenSpecification extends AbstractSpecification {
     dbRow(36, 'LNI', 'INITIAL', "'2019-07-01T00:00Z'", "'2019-07-20T00:00Z'", "'2019-07-29T00:00Z'", '2019-07-22', '2019-07-29', '2019-08-03', ic('R').build(), SECURITY_AUTO)
     dbRow(37, 'LNI', 'INITIAL', "'2019-07-01T00:00Z'", "'2019-07-20T00:00Z'", "'2019-07-29T00:00Z'", '2019-07-22', '2019-07-29', '2019-08-03', ic('T').securityType(SecurityType.MANUAL).build())
 
+
+    dbRow(70, 'PFI', 'INITIAL', "'2019-07-01Z00:00'", null, null, '2019-07-20', '2019-08-01', '2019-08-03', ic('I').build())
+    dbRow(71, 'PFI', 'INITIAL', "'2019-07-01Z00:00'", null, null, '2019-07-21', '2019-07-31', '2019-08-02', ic('I', 'J').build())
+
+    // PFI initial referred to security
+    dbRow(72, 'PFI', 'INITIAL', "'2019-07-01T03:00Z'", "'2019-07-09T15:30Z'", "'2019-07-09T17:40Z'", '2019-07-22', '2019-07-30', '2019-08-03', ic('I').build(), SECURITY_AUTO)
+    dbRow(73, 'PFI', 'INITIAL', "'2019-07-01T04:00Z'", "'2019-07-10T09:00Z'", "'2019-07-11T11:00Z'", '2019-07-23', '2019-07-29', '2019-07-28', ic('J', 'I').securityType(SecurityType.MANUAL).build())
+
+
     given: 'a supervisor is logged in'
     elite2Api.stubUncategorisedAwaitingApproval('PFI')
     prisonerSearchApi.stubSentenceData(['ON700'], [700], ['28/01/2019'])
@@ -113,14 +122,17 @@ class DashboardWomenSpecification extends AbstractSpecification {
     statsTypeOptions*.text().contains('all female prisons')
 
     then: 'The stats displayed are as follows'
-    numbersTableRows[0].find('td')*.text() == ['Closed', '', '50.0%', '2']
-    numbersTableRows[1].find('td')*.text() == ['Closed', 'Open', '25.0%', '1']
-    numbersTableRows[2].find('td')*.text() == ['Open', 'Open', '25.0%', '1']
+    numbersTableRows[0].find('td')*.text() == ['YOI closed', '', '25.0%', '2']
+    numbersTableRows[1].find('td')*.text() == ['YOI closed', 'YOI open', '12.5%', '1']
+    numbersTableRows[2].find('td')*.text() == ['YOI open', 'YOI closed', '12.5%', '1']
+    numbersTableRows[3].find('td')*.text() == ['Closed', '', '25.0%', '2']
+    numbersTableRows[4].find('td')*.text() == ['Closed', 'Open', '12.5%', '1']
+    numbersTableRows[5].find('td')*.text() == ['Open', 'Open', '12.5%', '1']
 
-    securityTableRows[0].find('td')*.text() == ['Manual', '1']
-    securityTableRows[1].find('td')*.text() == ['Automatic', '1']
+    securityTableRows[0].find('td')*.text() == ['Manual', '2']
+    securityTableRows[1].find('td')*.text() == ['Automatic', '2']
     securityTableRows[2].find('td')*.text() == ['Flagged', '0']
-    securityTableRows[3].find('td')*.text() == ['Total', '2']
+    securityTableRows[3].find('td')*.text() == ['Total', '4']
 
     timelineTableRows*.text() == [
       'Assessment started to sent to security 8.5 days',
@@ -129,9 +141,9 @@ class DashboardWomenSpecification extends AbstractSpecification {
       'Assessment started to approval complete 29.5 days']
 
     completionTableRows*.text() == [
-      'Before due date 75.0% 3',
-      'Late 25.0% 1',
-      'Total 4']
+      'Before due date 75.0% 6',
+      'Late 25.0% 2',
+      'Total 8']
 
     when: 'the user selects the whole estate'
     form.scope = 'all'
@@ -139,28 +151,32 @@ class DashboardWomenSpecification extends AbstractSpecification {
 
     then: 'the stats are as follows'
     at DashboardInitialPage
-    numbersTableRows[0].find('td')*.text() == ['Closed', '', '37.5%', '3']
-    numbersTableRows[1].find('td')*.text() == ['Closed', 'Closed', '12.5%', '1']
-    numbersTableRows[2].find('td')*.text() == ['Closed', 'Open', '25.0%', '2']
-    numbersTableRows[3].find('td')*.text() == ['Open', '', '12.5%', '1']
-    numbersTableRows[4].find('td')*.text() == ['Open', 'Open', '12.5%', '1']
+    numbersTableRows[0].find('td')*.text() == ['YOI closed', '', '16.7%', '2']
+    numbersTableRows[1].find('td')*.text() == ['YOI closed', 'YOI open', '8.3%', '1']
+    numbersTableRows[2].find('td')*.text() == ['YOI open', 'YOI closed', '8.3%', '1']
+    numbersTableRows[3].find('td')*.text() == ['Closed', '', '25.0%', '3']
+    numbersTableRows[4].find('td')*.text() == ['Closed', 'Closed', '8.3%', '1']
+    numbersTableRows[5].find('td')*.text() == ['Closed', 'Open', '16.7%', '2']
+    numbersTableRows[6].find('td')*.text() == ['Open', '', '8.3%', '1']
+    numbersTableRows[7].find('td')*.text() == ['Open', 'Open', '8.3%', '1']
 
-    securityTableRows[0].find('td')*.text() == ['Manual', '2']
-    securityTableRows[1].find('td')*.text() == ['Automatic', '2']
+    securityTableRows[0].find('td')*.text() == ['Manual', '3']
+    securityTableRows[1].find('td')*.text() == ['Automatic', '3']
     securityTableRows[2].find('td')*.text() == ['Flagged', '0']
-    securityTableRows[3].find('td')*.text() == ['Total', '4']
+    securityTableRows[3].find('td')*.text() == ['Total', '6']
 
     timelineTableRows*.text() == [
-      'Assessment started to sent to security 13.75 days',
-      'Sent to security to security review complete 4.75 days',
-      'Security review complete to approval complete 9.75 days',
-      'Assessment started to approval complete 28.75 days']
+      'Assessment started to sent to security 12 days',
+      'Sent to security to security review complete 3.33 days',
+      'Security review complete to approval complete 13 days',
+      'Assessment started to approval complete 29 days']
 
     completionTableRows*.text() == [
-      'Before due date 87.5% 7',
-      'Late 12.5% 1',
-      'Total 8']
+      'Before due date 83.3% 10',
+      'Late 16.7% 2',
+      'Total 12']
   }
+
 
   def "The recat dashboard should show correct women stats data"() {
 
@@ -193,6 +209,16 @@ class DashboardWomenSpecification extends AbstractSpecification {
     dbRow(60, 'LNI', 'RECAT', "'2019-07-01T00:00Z'", "'2019-07-01T08:00Z'", "'2019-07-19T00:00Z'", '2019-07-22', '2019-08-05', '2019-08-03', rc('R').build(), SECURITY_AUTO)
     dbRow(61, 'LNI', 'RECAT', "'2019-07-01T00:00Z'", "'2019-07-10T00:00Z'", "'2019-07-19T00:00Z'", '2019-07-22', '2019-07-29', '2019-08-03', rc('T', 'R').securityType(SecurityType.MANUAL).build())
 
+    dbRow(80, 'PFI', 'RECAT', "'2019-07-01T00:00Z'", "'2019-07-10T00:00Z'", "'2019-07-19T00:00Z'", '2019-07-22', '2019-08-05', '2019-08-03', rc('I').build(), SECURITY_AUTO)
+    dbRow(81, 'PFI', 'RECAT', "'2019-07-01T00:00Z'", "'2019-07-10T00:00Z'", "'2019-07-19T04:30Z'", '2019-07-22', '2019-07-29', '2019-08-03', rc('J', 'I').securityType(SecurityType.MANUAL).build())
+    dbRow(82, 'PFI', 'RECAT', "'2019-07-01T00:00Z'", "'2019-07-10T00:00Z'", "'2019-07-19T00:00Z'", '2019-07-22', '2019-07-29', '2019-08-03', rc('J').securityType(SecurityType.FLAGGED).build())
+
+    //-------------------------------------------------------------------------------------------------------------
+    // LNI recat
+    dbRow(90, 'LNI', 'RECAT', "'2019-07-01T00:00Z'", null,                  null,                  '2019-07-22', '2019-08-05', '2019-08-03', rc('J').build())
+    dbRow(91, 'LNI', 'RECAT', "'2019-07-01T00:00Z'", null,                  null,                  '2019-07-22', '2019-08-29', '2019-08-03', rc('I').build())
+
+
     given: 'a supervisor is logged in'
     elite2Api.stubUncategorisedAwaitingApproval('PFI')
     prisonerSearchApi.stubSentenceData(['ON700'], [700], ['28/01/2019'])
@@ -206,29 +232,30 @@ class DashboardWomenSpecification extends AbstractSpecification {
     statsTypeOptions*.text().contains('all female prisons')
 
     then: 'The stats displayed are as follows'
+    numbersTableRows[0].find('td')*.text() == ['YOI closed', '', '12.5%', '1']
+    numbersTableRows[1].find('td')*.text() == ['YOI open', '', '12.5%', '1']
+    numbersTableRows[2].find('td')*.text() == ['YOI open', 'YOI closed', '12.5%',  '1']
+    numbersTableRows[3].find('td')*.text() == ['Closed', '', '37.5%', '3']
+    numbersTableRows[4].find('td')*.text() == ['Closed', 'Open', '12.5%', '1']
+    numbersTableRows[5].find('td')*.text() == ['Open', 'Closed', '12.5%',  '1']
 
 
-    numbersTableRows[0].find('td')*.text() == ['Closed', '', '60.0%', '3']
-    numbersTableRows[1].find('td')*.text() == ['Closed', 'Open', '20.0%', '1']
-    numbersTableRows[2].find('td')*.text() == ['Open', 'Closed', '20.0%',  '1']
 
-
-
-    securityTableRows[0].find('td')*.text() == ['Manual', '1']
-    securityTableRows[1].find('td')*.text() == ['Automatic', '1']
-    securityTableRows[2].find('td')*.text() == ['Flagged', '1']
-    securityTableRows[3].find('td')*.text() == ['Total', '3']
+    securityTableRows[0].find('td')*.text() == ['Manual', '2']
+    securityTableRows[1].find('td')*.text() == ['Automatic', '2']
+    securityTableRows[2].find('td')*.text() == ['Flagged', '2']
+    securityTableRows[3].find('td')*.text() == ['Total', '6']
 
     timelineTableRows*.text() == [
       'Assessment started to sent to security 9 days',
       'Sent to security to security review complete 9 days',
       'Security review complete to approval complete 12.33 days',
-      'Assessment started to approval complete 30.8 days']
+      'Assessment started to approval complete 30.63 days']
 
     completionTableRows*.text() == [
-      'Before due date 60.0% 3',
-      'Late 40.0% 2',
-      'Total 5']
+      'Before due date 62.5% 5',
+      'Late 37.5% 3',
+      'Total 8']
 
     when: 'the user selects the whole estate'
     form.scope = 'all'
@@ -236,29 +263,32 @@ class DashboardWomenSpecification extends AbstractSpecification {
 
     then: 'the stats are as follows'
     at DashboardRecatPage
-    numbersTableRows[0].find('td')*.text() == ['Closed', '', '55.6%', '5']
-    numbersTableRows[1].find('td')*.text() == ['Closed', 'Open', '11.1%', '1']
-    numbersTableRows[2].find('td')*.text() == ['Open', '', '11.1%', '1']
-    numbersTableRows[3].find('td')*.text() == ['Open', 'Closed', '22.2%',  '2']
+    numbersTableRows[0].find('td')*.text() == ['YOI closed', '', '14.3%', '2']
+    numbersTableRows[1].find('td')*.text() == ['YOI open', '', '14.3%', '2']
+    numbersTableRows[2].find('td')*.text() == ['YOI open', 'YOI closed', '7.1%', '1']
+    numbersTableRows[3].find('td')*.text() == ['Closed', '', '35.7%', '5']
+    numbersTableRows[4].find('td')*.text() == ['Closed', 'Open', '7.1%', '1']
+    numbersTableRows[5].find('td')*.text() == ['Open', '', '7.1%', '1']
+    numbersTableRows[6].find('td')*.text() == ['Open', 'Closed', '14.3%',  '2']
 
-    securityTableRows[0].find('td')*.text() == ['Manual', '2']
-    securityTableRows[1].find('td')*.text() == ['Automatic', '2']
-    securityTableRows[2].find('td')*.text() == ['Flagged', '1']
-    securityTableRows[3].find('td')*.text() == ['Total', '5']
+    securityTableRows[0].find('td')*.text() == ['Manual', '3']
+    securityTableRows[1].find('td')*.text() == ['Automatic', '3']
+    securityTableRows[2].find('td')*.text() == ['Flagged', '2']
+    securityTableRows[3].find('td')*.text() == ['Total', '8']
 
     timelineTableRows*.text() == [
-      'Assessment started to sent to security 7.2 days',
-      'Sent to security to security review complete 10.8 days',
-      'Security review complete to approval complete 12.8 days',
-      'Assessment started to approval complete 34.56 days']
+      'Assessment started to sent to security 7.88 days',
+      'Sent to security to security review complete 10.13 days',
+      'Security review complete to approval complete 12.63 days',
+      'Assessment started to approval complete 35.43 days']
 
     completionTableRows*.text() == [
-      'Before due date 44.4% 4',
-      'Late 55.6% 5',
-      'Total 9']
+      'Before due date 42.9% 6',
+      'Late 57.1% 8',
+      'Total 14']
   }
 
-  def "The recat dashboard should show correct change table"() {
+  def "The recat dashboard should show correct change table for Women"() {
 
     db.doCreateCompleteRow(-10, 700, ic('R').build(), 'FEMALE_USER', 'AWAITING_APPROVAL', 'RECAT', null, null, null, 1, '{}',
       'PFI', "ON700", "'2019-07-01Z00:00'", null, null, '2019-07-29', '2019-07-22', '2019-08-03')
@@ -274,24 +304,24 @@ class DashboardWomenSpecification extends AbstractSpecification {
     dbCatRow(105, 1, '2019-02-15', 'PFI', 'R'); dbCatRow(105, 2, '2019-08-15', 'PFI', 'T')
     dbCatRow(106, 1, '2019-02-15', 'PFI', 'R'); dbCatRow(106, 2, '2019-08-15', 'PFI', 'T')
     dbCatRow(107, 1, '2019-02-15', 'PFI', 'R'); dbCatRow(107, 2, '2019-08-15', 'PFI', 'T')
-    dbCatRow(108, 1, '2019-02-15', 'PFI', 'R'); dbCatRow(108, 2, '2019-08-15', 'PFI', 'T')
+    dbCatRow(108, 1, '2019-02-15', 'PFI', 'J'); dbCatRow(108, 2, '2019-08-15', 'PFI', 'I')
     dbCatRow(109, 1, '2019-02-15', 'PFI', 'R'); dbCatRow(109, 2, '2019-08-14', 'PFI', 'R')
     dbCatRow(110, 1, '2019-02-15', 'PFI', 'T'); dbCatRow(110, 2, '2019-08-14', 'PFI', 'R')
-    dbCatRow(111, 1, '2019-02-15', 'PFI', 'T'); dbCatRow(111, 2, '2019-08-15', 'PFI', 'T')
+    dbCatRow(111, 1, '2019-02-15', 'PFI', 'J'); dbCatRow(111, 2, '2019-08-15', 'PFI', 'J')
     dbCatRow(112, 1, '2019-02-15', 'PFI', 'T'); dbCatRow(112, 2, '2019-08-15', 'PFI', 'T')
     dbCatRow(113, 1, '2019-02-15', 'PFI', 'T'); dbCatRow(113, 2, '2019-08-15', 'PFI', 'T')
     dbCatRow(114, 1, '2019-02-15', 'PFI', 'I'); dbCatRow(114, 2, '2019-08-15', 'PFI', 'J')
     dbCatRow(115, 1, '2019-02-15', 'PFI', 'I'); dbCatRow(115, 2, '2019-08-15', 'PFI', 'J')
-    dbCatRow(116, 1, '2019-02-15', 'PFI', 'I'); dbCatRow(116, 2, '2019-08-15', 'PFI', 'I')
+    dbCatRow(116, 1, '2019-02-15', 'PFI', 'I'); dbCatRow(116, 2, '2019-08-15', 'PFI', 'J')
     dbCatRow(117, 1, '2019-02-15', 'PFI', 'I'); dbCatRow(117, 2, '2019-08-15', 'PFI', 'I')
-    dbCatRow(118, 1, '2019-02-15', 'PFI', 'I'); dbCatRow(118, 2, '2019-08-15', 'PFI', 'I')
+    dbCatRow(118, 1, '2019-02-15', 'PFI', 'J'); dbCatRow(118, 2, '2019-08-15', 'PFI', 'I')
     dbCatRow(119, 1, '2019-02-15', 'PFI', 'T') // <- NOT ignored are previous category
-    dbCatRow(119, 2, '2019-05-15', 'PFI', 'R'); dbCatRow(119, 3, '2019-08-15', 'PFI', 'T')
+    dbCatRow(119, 2, '2019-05-15', 'PFI', 'J'); dbCatRow(119, 3, '2019-08-15', 'PFI', 'I')
     dbCatRow(120, 1, '2017-02-15', 'PFI', 'R'); dbCatRow(120, 2, '2017-08-15', 'PFI', 'T')
     // ignored when filtered:
-    dbCatRow(121, 1, '2017-08-15', 'LNI', 'T'); dbCatRow(121, 2, '2019-08-15', 'LNI', 'T')
+    dbCatRow(121, 1, '2017-08-15', 'LNI', 'J'); dbCatRow(121, 2, '2019-08-15', 'LNI', 'J')
     // no previous:
-    dbCatRow(122, 5, '2019-05-15', 'PFI', 'R')
+    dbCatRow(122, 5, '2019-05-15', 'PFI', 'J')
 
     // INITIAL R -> RECAT T
     dbCatRow(123, 1, '2016-02-15', 'PFI', 'R', null,'INITIAL'); dbCatRow(123, 2, '2016-08-15', 'PFI', 'T', null, 'RECAT')
@@ -306,11 +336,11 @@ class DashboardWomenSpecification extends AbstractSpecification {
     statsTypeOptions*.text().contains('all female prisons')
 
     then: 'The stats displayed are as follows'
-    reviewNumbersTableRows[0].find('td')*.text() == ['Open', '4', '3', '', '', '7']
-    reviewNumbersTableRows[1].find('td')*.text() == ['Closed', '10', '1', '', '', '11']
-    reviewNumbersTableRows[2].find('td')*.text() == ['YOI closed', '', '', '3', '2', '5']
-    reviewNumbersTableRows[3].find('td')*.text() == ['YOI open', '', '', '', '', '0']
-    reviewNumbersTableRows[4].find('td')*.text() == ['Total', '14', '4', '3', '2', '23']
+    reviewNumbersTableRows[0].find('td')*.text() == ['Open', '3', '2', '', '1', '6']
+    reviewNumbersTableRows[1].find('td')*.text() == ['Closed', '8', '1', '', '', '9']
+    reviewNumbersTableRows[2].find('td')*.text() == ['YOI closed', '', '', '1', '3', '4']
+    reviewNumbersTableRows[3].find('td')*.text() == ['YOI open', '', '', '3', '1', '4']
+    reviewNumbersTableRows[4].find('td')*.text() == ['Total', '11', '3', '4', '5', '23']
 
     when: 'the user filters to an end date'
     form.startDate = ''
@@ -319,7 +349,7 @@ class DashboardWomenSpecification extends AbstractSpecification {
 
     then: 'the results totals are now reduced'
     at DashboardRecatPage
-    reviewNumbersTableRows[4].find('td')*.text() == ['Total', '2', '3', '0', '0', '5']
+    reviewNumbersTableRows[4].find('td')*.text() == ['Total', '2', '2', '0', '1', '5']
 
     when: 'the user filters from a start date to an end date'
     form.startDate = '16/08/2017'
@@ -328,7 +358,7 @@ class DashboardWomenSpecification extends AbstractSpecification {
 
     then: 'the results totals are now reduced'
     at DashboardRecatPage
-    reviewNumbersTableRows[4].find('td')*.text() == ['Total', '0', '3', '0', '0', '3']
+    reviewNumbersTableRows[4].find('td')*.text() == ['Total', '0', '2', '0', '1', '3']
 
     when: 'the user filters a period when initial R was recategorised as T'
     form.startDate = '15/08/2016'
@@ -339,4 +369,5 @@ class DashboardWomenSpecification extends AbstractSpecification {
     at DashboardRecatPage
     reviewNumbersTableRows[4].find('td')*.text() == ['Total', '1', '0', '0', '0', '1']
   }
+
 }
