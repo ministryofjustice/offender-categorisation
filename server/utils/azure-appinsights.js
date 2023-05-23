c2ac5fe35e0741c43df76f9f01e99a5663d73bce
jonhindle <jon.hindle@digital.justice.gov.uk>
2023-05-23 11:24:47 +0100

    SI-12-establishment-usage-tracking

--- server/utils/azure-appinsights.js
+++ server/utils/azure-appinsights.js
@@ -1,14 +1,43 @@
-const appInsights = require('applicationinsights')
+const { setup, defaultClient, DistributedTracingModes, Contracts } = require('applicationinsights')
 const applicationVersion = require('../application-version')
 
-const { packageData, buildNumber } = applicationVersion
-if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
-  // eslint-disable-next-line no-console
-  console.log('Enabling azure application insights')
-  appInsights.setup().setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C).start()
-  module.exports = appInsights.defaultClient
-  appInsights.defaultClient.context.tags['ai.cloud.role'] = `${packageData.name}`
-  appInsights.defaultClient.context.tags['ai.application.ver'] = buildNumber
-} else {
-  module.exports = null
+const initialiseAppInsights = () => {
+  if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
+    // eslint-disable-next-line no-console
+    console.log('Enabling azure application insights')
+    setup().setDistributedTracingMode(DistributedTracingModes.AI_AND_W3C).start()
+  }
 }
+
+const version = () => {
+  const { buildNumber } = applicationVersion
+  return buildNumber
+}
+
+const buildAppInsightsClient = (name = applicationVersion.packageData.name) => {
+  if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
+    defaultClient.context.tags['ai.cloud.role'] = `${name}`
+    defaultClient.context.tags['ai.application.ver'] = version()
+    return defaultClient
+  }
+  return null
+}
+
+const addUserDataToRequests = (envelope, contextObjects) => {
+  const isRequest = envelope.data.baseType === Contracts.TelemetryTypeString.Request
+  if (isRequest) {
+    const { username, activeCaseLoadId } = contextObjects?.['http.ServerRequest']?.res?.locals?.user || {}
+    if (username) {
+      const { properties } = envelope.data.baseData
+      // eslint-disable-next-line no-param-reassign
+      envelope.data.baseData.properties = {
+        ...properties,
+        username,
+        activeCaseLoadId,
+      }
+    }
+  }
+  return true
+}
+
+module.exports = { initialiseAppInsights, buildAppInsightsClient, addUserDataToRequests }
