ea5b58892c96b27c4039f5dd0ee1a549a82ba6fa
Christopher Moss <chris@a6software.co.uk>
2023-06-26 13:31:43 +0100

    [SI-108] Adds lite category button disabling JS - implementation only (#719)

---
+++ assets/js/preventDoubleSubmit/index.js
@@ -0,0 +1,15 @@
+;(() => {
+  'use strict'
+
+  const disableSubmitButtons = event => {
+    console.log('event', event.currentTarget)
+    event.currentTarget.querySelectorAll('button[data-prevent-double-click="true"]').forEach(button => {
+      button.dataset.clicked = 'true'
+      button.disabled = true
+      button.setAttribute('aria-disabled', 'true')
+      button.classList.add('govuk-button--disabled')
+    })
+  }
+
+  document.querySelectorAll('form').forEach(form => form.addEventListener('submit', disableSubmitButtons))
+})()
