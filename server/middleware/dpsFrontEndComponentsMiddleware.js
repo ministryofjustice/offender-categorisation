e1d36cff93fc5ad0547a61cc035d140e208b91aa
Christopher Moss <christopher.moss@digital.justice.gov.uk>
2024-02-02 09:19:32 +0000

    [SI-676] Reduce logging noise for logged out users requested DPS FE components

--- server/middleware/dpsFrontEndComponentsMiddleware.js
+++ server/middleware/dpsFrontEndComponentsMiddleware.js
@@ -3,16 +3,18 @@ const logger = require('../../log')
 module.exports = function getFrontendComponents(frontEndComponentsService) {
   return async (req, res, next) => {
     try {
-      const [header, footer] = await Promise.all([
-        frontEndComponentsService.getComponent('header', res.locals?.user?.token),
-        frontEndComponentsService.getComponent('footer', res.locals?.user?.token),
-      ])
+      if (res.locals?.user?.token) {
+        const [header, footer] = await Promise.all([
+          frontEndComponentsService.getComponent('header', res.locals?.user?.token),
+          frontEndComponentsService.getComponent('footer', res.locals?.user?.token),
+        ])
 
-      res.locals.feComponents = {
-        header: header.html,
-        footer: footer.html,
-        cssIncludes: [...header.css, ...footer.css],
-        jsIncludes: [...header.javascript, ...footer.javascript],
+        res.locals.feComponents = {
+          header: header.html,
+          footer: footer.html,
+          cssIncludes: [...header.css, ...footer.css],
+          jsIncludes: [...header.javascript, ...footer.javascript],
+        }
       }
 
       next()
