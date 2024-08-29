7e1641b678be5ccc8f63289c97953b9da14442cd
timothy-ness <timothy.ness@digital.justice.gov.uk>
2024-08-29 08:53:46 +0100

    added in more tests

--- integration_tests/pages/recategoriser/base.ts
+++ integration_tests/pages/recategoriser/base.ts
@@ -2,4 +2,9 @@ import Page, { PageElement } from '../page'
 
 export default abstract class BaseRecategoriserPage extends Page {
   noResultsDiv = (): PageElement => cy.get('#no-results-message')
+  hideFilterButton = (): PageElement => cy.get('#hideFilterButton')
+  filterContainer = (): PageElement => cy.get('#filterContainer')
+  applyFiltersButton = (): PageElement => cy.get('#applyFilters')
+  selectAllFiltersButton = (): PageElement => cy.get('#selectAllSuitabilityForOpenConditionsFilter')
+  lowRiskOfEscapeFilterCheckbox = (): PageElement => cy.get('[data-qa=lowRiskOfEscape_checkbox]')
 }
