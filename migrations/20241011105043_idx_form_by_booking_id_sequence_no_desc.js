dd13b3ddd7e4d3689f1446e39de7b579fecc2a76
Christopher Moss <christopher.moss@digital.justice.gov.uk>
2024-10-11 13:33:48 +0100

    [SI-1261] Add index for form filtering, and sorting

--- migrations/20241011105043_idx_form_by_booking_id_sequence_no_desc.js
+++ migrations/20241011105043_idx_form_by_booking_id_sequence_no_desc.js
@@ -1,9 +1,5 @@
 exports.up = knex =>
-  knex.schema.table('form', table => {
-    table.index(['offender_no', 'status', 'sequence_no'], 'idx_form_offender_no_status_sequence_no')
-  })
+  knex.schema.raw('CREATE INDEX idx_form_booking_id_sequence_no_desc ON form (booking_id, sequence_no DESC)')
 
-exports.down = knex =>
-  knex.schema.table('form', table => {
-    table.dropIndex(['offender_no', 'status', 'sequence_no'], 'idx_form_offender_no_status_sequence_no')
-  })
+exports.down = knex => knex.schema.raw('DROP INDEX idx_form_booking_id_sequence_no_desc')
+g
