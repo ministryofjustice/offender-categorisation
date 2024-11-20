8e825aadd8b58687eff5ee54e363be39e8e9b072
timothy-ness <timothy.ness@digital.justice.gov.uk>
2024-11-20 08:21:15 +0000

    code review changes

--- server/middleware/bookingExistsAtCurrentEstablishmentMiddleware.test.ts
+++ server/middleware/bookingExistsAtCurrentEstablishmentMiddleware.test.ts
@@ -1,5 +1,4 @@
-import type { Response } from 'express'
-import { getMockReq } from '@jest-mock/express'
+import { getMockReq, getMockRes } from '@jest-mock/express'
 import { bookingExistsAtCurrentEstablishmentMiddleware } from './bookingExistsAtCurrentEstablishmentMiddleware'
 import { makeTestUserDto } from '../services/user/user.dto.test-factory'
 import { makeTestBasicOffenderDetailsDto } from '../services/offender/basicOffenderDetails.dto.test-factory'
@@ -10,31 +9,20 @@ const mockUserService = {
 const mockOffenderService = {
   getBasicOffenderDetails: jest.fn(),
 }
+const { res, clearMockRes } = getMockRes({ locals: {} })
 const testAgencyId = 'TEST'
 const testParams = { bookingId: '123456' }
 afterEach(() => {
   jest.resetAllMocks()
+  clearMockRes()
 })
 
 describe('bookingExistsAtCurrentEstablishmentMiddleware', () => {
   const next = jest.fn()
   const middleware = bookingExistsAtCurrentEstablishmentMiddleware(mockUserService, mockOffenderService)
 
-  test('it works correctly with matching agency IDs', async () => {
-    mockUserService.getUser.mockResolvedValue(makeTestUserDto({ activeCaseLoad: { caseLoadId: testAgencyId } }))
-    mockOffenderService.getBasicOffenderDetails.mockResolvedValue(
-      makeTestBasicOffenderDetailsDto({ agencyId: testAgencyId })
-    )
-
-    await middleware(getMockReq({ params: testParams }), { locals: {} } as Response, next)
-
-    expect(mockUserService.getUser).toBeCalledTimes(1)
-    expect(mockOffenderService.getBasicOffenderDetails).toBeCalledTimes(1)
-    expect(next).toBeCalledTimes(1)
-  })
-
   test('it fails when there is no booking ID', async () => {
-    await middleware(getMockReq({ params: {} }), { locals: {} } as Response, next)
+    await middleware(getMockReq(), res, next)
 
     expect(mockUserService.getUser).not.toHaveBeenCalled()
     expect(mockOffenderService.getBasicOffenderDetails).not.toHaveBeenCalled()
@@ -42,50 +30,60 @@ describe('bookingExistsAtCurrentEstablishmentMiddleware', () => {
     expect(next).toBeCalledWith(new Error('bookingExistsAtCurrentEstablishmentMiddleware: No booking id in params'))
   })
 
-  test('it fails when the agency IDs do not match', async () => {
-    mockUserService.getUser.mockResolvedValue(makeTestUserDto({ activeCaseLoad: { caseLoadId: testAgencyId } }))
-    mockOffenderService.getBasicOffenderDetails.mockResolvedValue(makeTestBasicOffenderDetailsDto({ agencyId: 'BLA' }))
-    await middleware(getMockReq({ params: testParams }), { locals: {} } as Response, next)
+  describe('with booking ID', () => {
+    afterEach(() => {
+      expect(mockUserService.getUser).toBeCalledTimes(1)
+      expect(mockOffenderService.getBasicOffenderDetails).toBeCalledTimes(1)
+      expect(next).toBeCalledTimes(1)
+    })
 
-    expect(mockUserService.getUser).toBeCalledTimes(1)
-    expect(mockOffenderService.getBasicOffenderDetails).toBeCalledTimes(1)
-    expect(next).toBeCalledTimes(1)
-    expect(next).toBeCalledWith(
-      new Error(
-        'bookingExistsAtCurrentEstablishmentMiddleware: booking agency ID does not match to the active caseload'
+    test('it works correctly with matching agency IDs', async () => {
+      mockUserService.getUser.mockResolvedValue(makeTestUserDto({ activeCaseLoad: { caseLoadId: testAgencyId } }))
+      mockOffenderService.getBasicOffenderDetails.mockResolvedValue(
+        makeTestBasicOffenderDetailsDto({ agencyId: testAgencyId })
       )
-    )
-  })
 
-  test('it handles undefined response for getUser', async () => {
-    mockUserService.getUser.mockResolvedValue(undefined)
-    mockOffenderService.getBasicOffenderDetails.mockResolvedValue(
-      makeTestBasicOffenderDetailsDto({ agencyId: testAgencyId })
-    )
-    await middleware(getMockReq({ params: testParams }), { locals: {} } as Response, next)
+      await middleware(getMockReq({ params: testParams }), res, next)
+    })
 
-    expect(mockUserService.getUser).toBeCalledTimes(1)
-    expect(mockOffenderService.getBasicOffenderDetails).toBeCalledTimes(1)
-    expect(next).toBeCalledTimes(1)
-    expect(next).toBeCalledWith(
-      new Error(
-        'bookingExistsAtCurrentEstablishmentMiddleware: booking agency ID does not match to the active caseload'
+    test('it fails when the agency IDs do not match', async () => {
+      mockUserService.getUser.mockResolvedValue(makeTestUserDto({ activeCaseLoad: { caseLoadId: testAgencyId } }))
+      mockOffenderService.getBasicOffenderDetails.mockResolvedValue(
+        makeTestBasicOffenderDetailsDto({ agencyId: 'BLA' })
       )
-    )
-  })
+      await middleware(getMockReq({ params: testParams }), res, next)
 
-  test('it handles undefined response for getBasicOffenderDwetails', async () => {
-    mockUserService.getUser.mockResolvedValue(makeTestUserDto({ activeCaseLoad: { caseLoadId: testAgencyId } }))
-    mockOffenderService.getBasicOffenderDetails.mockResolvedValue(undefined)
-    await middleware(getMockReq({ params: testParams }), { locals: {} } as Response, next)
-
-    expect(mockUserService.getUser).toBeCalledTimes(1)
-    expect(mockOffenderService.getBasicOffenderDetails).toBeCalledTimes(1)
-    expect(next).toBeCalledTimes(1)
-    expect(next).toBeCalledWith(
-      new Error(
-        'bookingExistsAtCurrentEstablishmentMiddleware: booking agency ID does not match to the active caseload'
+      expect(next).toBeCalledWith(
+        new Error(
+          'bookingExistsAtCurrentEstablishmentMiddleware: booking agency ID does not match to the active caseload'
+        )
       )
-    )
+    })
+
+    test('it handles undefined response for getUser', async () => {
+      mockUserService.getUser.mockResolvedValue(undefined)
+      mockOffenderService.getBasicOffenderDetails.mockResolvedValue(
+        makeTestBasicOffenderDetailsDto({ agencyId: testAgencyId })
+      )
+      await middleware(getMockReq({ params: testParams }), res, next)
+
+      expect(next).toBeCalledWith(
+        new Error(
+          'bookingExistsAtCurrentEstablishmentMiddleware: booking agency ID does not match to the active caseload'
+        )
+      )
+    })
+
+    test('it handles undefined response for getBasicOffenderDetails', async () => {
+      mockUserService.getUser.mockResolvedValue(makeTestUserDto({ activeCaseLoad: { caseLoadId: testAgencyId } }))
+      mockOffenderService.getBasicOffenderDetails.mockResolvedValue(undefined)
+      await middleware(getMockReq({ params: testParams }), res, next)
+
+      expect(next).toBeCalledWith(
+        new Error(
+          'bookingExistsAtCurrentEstablishmentMiddleware: booking agency ID does not match to the active caseload'
+        )
+      )
+    })
   })
 })
