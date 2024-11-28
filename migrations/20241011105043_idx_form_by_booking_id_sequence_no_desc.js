1a2642fbaab6af9bcf3944dbd6b75b05b0961aee
Christopher Moss <chris@a6software.co.uk>
2024-11-28 13:58:05 +0000

    [SI-1261] Add index for form filtering, and sorting (#853)
    * [SI-1261] Add index for form filtering, and sorting
    
    * [SI-1261] Add index for form filtering, and sorting
    
    * [SI-1261] Add index for form filtering, and sorting
    
    ---------
    
    Co-authored-by: timothy-ness <160499954+timothy-ness@users.noreply.github.com>

---
+++ migrations/20241011105043_idx_form_by_booking_id_sequence_no_desc.js
@@ -0,0 +1,4 @@
+exports.up = knex =>
+  knex.schema.raw('CREATE INDEX idx_form_booking_id_sequence_no_desc ON form (booking_id, sequence_no DESC)')
+
+exports.down = knex => knex.schema.raw('DROP INDEX idx_form_booking_id_sequence_no_desc')
