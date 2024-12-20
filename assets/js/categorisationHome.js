d588ea54df341e41c15cf478c532df83dc34ff68
timothy-ness <160499954+timothy-ness@users.noreply.github.com>
2024-12-20 07:48:20 +0000

    Si 1366 filters initial cat (#883)
    * added in side filters to the categorisation home page
    
    * lint issue
    
    * offenderProfile is possibly undefined
    
    * updated tests
    
    * removed console log
    
    * added in removing filter removed query param
    
    * code review

---
+++ assets/js/categorisationHome.js
@@ -0,0 +1,34 @@
+const hideFilterButton = document.getElementById('hideFilterButton')
+if (hideFilterButton) {
+  hideFilterButton.addEventListener('click', async event => {
+    let hideFilter = false
+    const filterContainer = document.getElementById('filterContainer')
+    const tableContainer = document.getElementById('tableContainer')
+    const button = event.target
+    if (button.innerText === 'Hide filter') {
+      filterContainer.classList.add('govuk-visually-hidden')
+      tableContainer.classList.remove('filterable-table-body-container')
+      tableContainer.classList.add('govuk-grid-column-full')
+      tableContainer.classList.add('govuk-!-padding-0')
+      // eslint-disable-next-line no-param-reassign
+      event.target.innerText = 'Show filter'
+      hideFilter = true
+    } else {
+      tableContainer.classList.remove('govuk-grid-column-full')
+      tableContainer.classList.remove('govuk-!-padding-0')
+      tableContainer.classList.add('filterable-table-body-container')
+      filterContainer.classList.remove('govuk-visually-hidden')
+      // eslint-disable-next-line no-param-reassign
+      event.target.innerText = 'Hide filter'
+    }
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+    await fetch('/categoriserHome/hide-filter', {
+      method: 'POST',
+      body: JSON.stringify({ hideFilter }),
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': csrfToken,
+      },
+    })
+  })
+}
