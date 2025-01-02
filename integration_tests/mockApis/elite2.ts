750972d7399a2fa288680772c5fbac2c0664ee76
timothy-ness <timothy.ness@digital.justice.gov.uk>
2025-01-02 14:20:24 +0000

    removed un-needed change

--- integration_tests/mockApis/elite2.ts
+++ integration_tests/mockApis/elite2.ts
@@ -1256,7 +1256,7 @@ const stubUncategorisedFull = (): SuperAgentRequest =>
   })
 
 const stubUncategorisedAwaitingApproval = (
-  { emptyResponse, nextReviewDate }: { emptyResponse: boolean, nextReviewDate: string } = { emptyResponse: false, nextReviewDate: '2025-01-01' }
+  { emptyResponse }: { emptyResponse: boolean } = { emptyResponse: false }
 ): SuperAgentRequest =>
   stubFor({
     request: {
@@ -1279,7 +1279,7 @@ const stubUncategorisedAwaitingApproval = (
               categoriserFirstName: 'ROGER',
               categoriserLastName: 'RABBIT',
               assessmentSeq: 4,
-              nextReviewDate: nextReviewDate,
+              nextReviewDate: '2025-01-01',
             },
             {
               bookingId: 12,
@@ -1291,7 +1291,7 @@ const stubUncategorisedAwaitingApproval = (
               categoriserFirstName: 'BUGS',
               categoriserLastName: 'BUNNY',
               assessmentSeq: 5,
-              nextReviewDate: nextReviewDate,
+              nextReviewDate: '2025-02-02',
             },
           ],
     },
