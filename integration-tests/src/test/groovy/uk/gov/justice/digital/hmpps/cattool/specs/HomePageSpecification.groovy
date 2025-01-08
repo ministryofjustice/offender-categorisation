113227dc6501b3716502610e1e747cdf5fa88895
timothy-ness <timothy.ness@digital.justice.gov.uk>
2025-01-08 09:46:57 +0000

    removed duplicate line

--- integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/specs/HomePageSpecification.groovy
+++ integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/specs/HomePageSpecification.groovy
@@ -177,7 +177,7 @@ class HomePageSpecification extends AbstractSpecification {
     at CategoriserHomePage
     elite2Api.stubGetOffenderDetails(678, "ON678")
     riskProfilerApi.stubForTasklists('ON678', 'C', false)
-    startButtons[0].click() // selects B2345YZ
+    selectFirstPrisoner() // selects B2345YZ
     at(new TasklistPage(bookingId: '678'))
     headerValue*.text() == ['ON678', '17/02/1970', 'C-04-02', 'Coventry', 'A Felony', 'Another Felony', 'Latvian', '02/02/2020']
     elite2Api.stubAssessments(['ON678'])
