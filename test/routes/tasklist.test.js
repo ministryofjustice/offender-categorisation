ba177047a3c28c36caa5c9e5047e54454764e81b
timothy-ness <timothy.ness@digital.justice.gov.uk>
2025-01-29 09:15:16 +0000

    removed unused import

--- test/routes/tasklist.test.js
+++ test/routes/tasklist.test.js
@@ -1,5 +1,4 @@
 const request = require('supertest')
-const moment = require('moment')
 const appSetup = require('./utils/appSetup')
 const createRouter = require('../../server/routes/tasklist')
 const { authenticationMiddleware } = require('./utils/mockAuthentication')
