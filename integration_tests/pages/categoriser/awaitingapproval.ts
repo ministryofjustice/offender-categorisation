a600a4edffaa46e842e72b1a2687832b7d354637
raghavender.tangallapally <raghavender.tangallapally@justice.gov.uk>
2025-01-31 09:14:30 +0000

    SI-1422 Open conditions initial cat cypress test

---
+++ integration_tests/pages/categoriser/awaitingapproval.ts
@@ -0,0 +1,12 @@
+import BaseCategoriserPage from './base'
+import {PageElement} from "../page";
+
+export default class CategoriserAwaitingApprovalViewPage extends BaseCategoriserPage {
+  static baseUrl: string = '/form/awaitingApprovalView'
+
+  constructor() {
+    super('Provisional categorisation')
+  }
+
+  warning = (): PageElement => cy.get('div.govuk-warning-text')
+}
