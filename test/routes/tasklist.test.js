b448bcdd956a90947d96665412e52fab2e2df2e0
timothy-ness <timothy.ness@digital.justice.gov.uk>
2025-02-05 08:36:57 +0000

    uncommented out code

--- test/routes/tasklist.test.js
+++ test/routes/tasklist.test.js
@@ -87,28 +87,28 @@ afterEach(() => {
 })
 
 describe('GET /tasklist/', () => {
-  // test('should render a tasklist for male prison', () => {
-  //   return request(app)
-  //     .get('/12345')
-  //     .expect(200)
-  //     .expect('Content-Type', /html/)
-  //     .expect(res => {
-  //       expect(res.text).toMatch(/Digital Prison Services.+Categorisation dashboard/s)
-  //       expect(res.text).toContain('Conditional Release Date')
-  //       expect(res.text).toContain('04/04/2020')
-  //       expect(res.text).toContain('Categorisation task list')
-  //       expect(res.text).toContain('Offending history')
-  //       expect(res.text).toContain('Not yet checked')
-  //       expect(res.text).toContain('Further charges')
-  //       expect(res.text).toContain('Safety and good order')
-  //       expect(res.text).toContain('Risk of escape')
-  //       expect(res.text).toContain('Extremism')
-  //       expect(res.text).toContain('Security information')
-  //       expect(res.text).not.toContain('Category decision')
-  //       expect(res.text).toContain('Set next category review date')
-  //       expect(res.text).toContain('Review and categorisation')
-  //     })
-  // })
+  test('should render a tasklist for male prison', () => {
+    return request(app)
+      .get('/12345')
+      .expect(200)
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        expect(res.text).toMatch(/Digital Prison Services.+Categorisation dashboard/s)
+        expect(res.text).toContain('Conditional Release Date')
+        expect(res.text).toContain('04/04/2020')
+        expect(res.text).toContain('Categorisation task list')
+        expect(res.text).toContain('Offending history')
+        expect(res.text).toContain('Not yet checked')
+        expect(res.text).toContain('Further charges')
+        expect(res.text).toContain('Safety and good order')
+        expect(res.text).toContain('Risk of escape')
+        expect(res.text).toContain('Extremism')
+        expect(res.text).toContain('Security information')
+        expect(res.text).not.toContain('Category decision')
+        expect(res.text).toContain('Set next category review date')
+        expect(res.text).toContain('Review and categorisation')
+      })
+  })
 
   test('should render a tasklist for female prison', () => {
     offendersService.getOffenderDetails.mockResolvedValue({
@@ -149,89 +149,88 @@ describe('GET /tasklist/', () => {
         expect(res.text).toContain('Review and categorisation')
       })
   })
-})
 
-//   test('should display automatically referred to security for SECURITY_AUTO status', () => {
-//     const today = moment().format('DD/MM/YYYY')
-//     const todayISO = moment().format('YYYY-MM-DD')
-//     offendersService.getOffenderDetails.mockResolvedValue({
-//       bookingId: 12345,
-//       displayName: 'Claire Dent',
-//       agencyId: 'MDI',
-//     })
-//     formService.createOrRetrieveCategorisationRecord.mockResolvedValue({
-//       id: 1111,
-//       formObject: { sample: 'string' },
-//       status: 'STARTED',
-//     })
-//     formService.getCategorisationRecord.mockResolvedValue({
-//       status: 'SECURITY_AUTO',
-//       securityReferredDate: `${todayISO}`,
-//     })
-//     const sampleSocProfile = {
-//       transferToSecurity: true,
-//       provisionalCategorisation: 'B',
-//     }
-//     const sampleExtremismProfile = {
-//       provisionalCategorisation: 'B',
-//     }
-//     riskProfilerService.getSecurityProfile.mockResolvedValue(sampleSocProfile)
-//     riskProfilerService.getExtremismProfile.mockResolvedValue(sampleExtremismProfile)
-//     formService.getCategorisationRecord.mockResolvedValue({
-//       id: 1111,
-//       securityReferredDate: `${todayISO}`,
-//       formObject: { sample: 'string', socProfile: sampleSocProfile },
-//       status: 'SECURITY_AUTO',
-//     })
-//
-//     return request(app)
-//       .get('/12345')
-//       .expect(200)
-//       .expect('Content-Type', /html/)
-//       .expect(res => {
-//         expect(res.text).toContain('Categorisation task list')
-//         expect(res.text).toContain(`Automatically referred to Security (${today})`)
-//         expect(res.text).toContain('href="/form/ratings/offendingHistory/12345"')
-//
-//         expect(formService.mergeRiskProfileData).toBeCalledWith(
-//           '12345',
-//           {
-//             socProfile: sampleSocProfile,
-//             extremismProfile: sampleExtremismProfile,
-//           },
-//           mockTransactionalClient
-//         )
-//         expect(formService.referToSecurityIfRiskAssessed).toBeCalledWith(
-//           '12345',
-//           'CA_USER_TEST',
-//           sampleSocProfile,
-//           sampleExtremismProfile,
-//           'STARTED',
-//           mockTransactionalClient
-//         )
-//         expect(formService.updateFormData).not.toBeCalled()
-//       })
-//   })
-//
-//   test('should not display referred to security for other status', () => {
-//     formService.referToSecurityIfRiskAssessed.mockResolvedValue('STARTED')
-//     return request(app)
-//       .get('/12345')
-//       .expect(200)
-//       .expect('Content-Type', /html/)
-//       .expect(res => {
-//         expect(res.text).toContain('Categorisation task list')
-//         expect(res.text).not.toContain(`Automatically referred to Security`)
-//         expect(formService.referToSecurityIfRiskAssessed).toBeCalledTimes(1)
-//       })
-//   })
-//
-//   test('should render categoriserSubmitted page', () =>
-//     request(app)
-//       .get('/categoriserSubmitted/12345')
-//       .expect(200)
-//       .expect('Content-Type', /html/)
-//       .expect(res => {
-//         expect(res.text).toContain('Submitted for approval')
-//       }))
-// })
+  test('should display automatically referred to security for SECURITY_AUTO status', () => {
+    const today = moment().format('DD/MM/YYYY')
+    const todayISO = moment().format('YYYY-MM-DD')
+    offendersService.getOffenderDetails.mockResolvedValue({
+      bookingId: 12345,
+      displayName: 'Claire Dent',
+      agencyId: 'MDI',
+    })
+    formService.createOrRetrieveCategorisationRecord.mockResolvedValue({
+      id: 1111,
+      formObject: { sample: 'string' },
+      status: 'STARTED',
+    })
+    formService.getCategorisationRecord.mockResolvedValue({
+      status: 'SECURITY_AUTO',
+      securityReferredDate: `${todayISO}`,
+    })
+    const sampleSocProfile = {
+      transferToSecurity: true,
+      provisionalCategorisation: 'B',
+    }
+    const sampleExtremismProfile = {
+      provisionalCategorisation: 'B',
+    }
+    riskProfilerService.getSecurityProfile.mockResolvedValue(sampleSocProfile)
+    riskProfilerService.getExtremismProfile.mockResolvedValue(sampleExtremismProfile)
+    formService.getCategorisationRecord.mockResolvedValue({
+      id: 1111,
+      securityReferredDate: `${todayISO}`,
+      formObject: { sample: 'string', socProfile: sampleSocProfile },
+      status: 'SECURITY_AUTO',
+    })
+
+    return request(app)
+      .get('/12345')
+      .expect(200)
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        expect(res.text).toContain('Categorisation task list')
+        expect(res.text).toContain(`Automatically referred to Security (${today})`)
+        expect(res.text).toContain('href="/form/ratings/offendingHistory/12345"')
+
+        expect(formService.mergeRiskProfileData).toBeCalledWith(
+          '12345',
+          {
+            socProfile: sampleSocProfile,
+            extremismProfile: sampleExtremismProfile,
+          },
+          mockTransactionalClient
+        )
+        expect(formService.referToSecurityIfRiskAssessed).toBeCalledWith(
+          '12345',
+          'CA_USER_TEST',
+          sampleSocProfile,
+          sampleExtremismProfile,
+          'STARTED',
+          mockTransactionalClient
+        )
+        expect(formService.updateFormData).not.toBeCalled()
+      })
+  })
+
+  test('should not display referred to security for other status', () => {
+    formService.referToSecurityIfRiskAssessed.mockResolvedValue('STARTED')
+    return request(app)
+      .get('/12345')
+      .expect(200)
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        expect(res.text).toContain('Categorisation task list')
+        expect(res.text).not.toContain(`Automatically referred to Security`)
+        expect(formService.referToSecurityIfRiskAssessed).toBeCalledTimes(1)
+      })
+  })
+
+  test('should render categoriserSubmitted page', () =>
+    request(app)
+      .get('/categoriserSubmitted/12345')
+      .expect(200)
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        expect(res.text).toContain('Submitted for approval')
+      }))
+})
