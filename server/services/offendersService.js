eb82d96a9067d73edcb7c699adef001eb21f72e6
timothy-ness <timothy.ness@digital.justice.gov.uk>
2025-02-24 11:33:34 +0000

    fixed test

--- server/services/offendersService.js
+++ server/services/offendersService.js
@@ -776,7 +776,7 @@ module.exports = function createOffendersService(
           moment(prisonerSearchRecord.sentenceStartDate).isAfter(moment(nomisRecord.assessmentDate))
         ) {
           logger.info(
-            `recategorisationDashboardErrorInvestigation: ${nomisRecord.offenderNo}, assessmentDate = ${nomisRecord.assessmentDate}, sentence date = ${prisonerSearchRecord.sentenceStartDate}, next review date = ${nomisRecord.nextReviewDate}, legalStatus = ${prisonerSearchRecord.legalStatus}, recall = ${prisonerSearchRecord.recall}`
+            `recategorisationDashboardErrorInvestigation: ${nomisRecord.offenderNo}, assessmentDate = ${nomisRecord.assessmentDate}, sentence date = ${prisonerSearchRecord?.sentenceStartDate}, next review date = ${nomisRecord.nextReviewDate}, legalStatus = ${prisonerSearchRecord?.legalStatus}, recall = ${prisonerSearchRecord?.recall}`
           )
         }
 
