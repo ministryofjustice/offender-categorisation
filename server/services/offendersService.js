f496723bdab49b47f2a055a3c10b54888abfa5ab
raghavender.tangallapally <raghavender.tangallapally@justice.gov.uk>
2025-02-26 08:32:11 +0000

    SI-1495 - Overdue text

--- server/services/offendersService.js
+++ server/services/offendersService.js
@@ -821,6 +821,7 @@ module.exports = function createOffendersService(
           reason,
           nextReviewDateDisplay: dateConverter(nomisRecord.nextReviewDate),
           overdue: isReviewOverdue(nomisRecord.nextReviewDate),
+          overdueText: getOverdueText(nomisRecord.nextReviewDate),
           dbRecordExists: decorated.dbRecordExists,
           pnomis,
           buttonText,
@@ -988,6 +989,7 @@ module.exports = function createOffendersService(
           reason,
           nextReviewDateDisplay,
           overdue: isReviewOverdue(nextReviewDate),
+          overdueText: getOverdueText(nextReviewDate),
           dbRecordExists: decorated.dbRecordExists,
           pnomis,
           buttonText,
@@ -1117,9 +1119,25 @@ module.exports = function createOffendersService(
     const actualDays = get10BusinessDays(sentenceDateMoment)
     const dateRequiredRaw = sentenceDateMoment.add(actualDays, 'day')
     const dateRequired = dateRequiredRaw.format('DD/MM/YYYY')
+    const now = moment(0, 'HH')
+    const overdue = dateRequiredRaw.isBefore(now)
+    const overdueText = getOverdueText(dateRequiredRaw)
+    return { daysSinceSentence, dateRequired, sentenceDate, overdue, overdueText }
+  }
 
-    const overdue = dateRequiredRaw.isBefore(moment(0, 'HH'))
-    return { daysSinceSentence, dateRequired, sentenceDate, overdue }
+  function getOverdueText(dateRequired) {
+    const diffInDays = moment(0, 'HH').diff(dateRequired, 'days')
+    let overdueText = ''
+    // eslint-disable-next-line default-case
+    switch (true) {
+      case diffInDays > 1:
+        overdueText = `${diffInDays} days overdue`
+        break
+      case diffInDays === 1:
+        overdueText = '1 day overdue'
+        break
+    }
+    return overdueText
   }
 
   async function decorateWithCategorisationData(offender, user, nomisClient, categorisation) {
