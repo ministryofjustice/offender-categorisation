8482c61ac4a6151aebcb8acafa40add540ec0d46
timothy-ness <timothy.ness@digital.justice.gov.uk>
2025-03-03 11:17:09 +0000

    updated date sorting based on discussion with devs

--- server/services/offendersService.js
+++ server/services/offendersService.js
@@ -379,10 +379,7 @@ module.exports = function createOffendersService(
             sentenceAndDate = entry && buildSentenceData(entry.sentenceDate)
           } else {
             const nomisCat = nomisCatData.find(record => record.bookingId === o.bookingId)
-            sentenceAndDate = {
-              dateRequired: nomisCat && dateConverter(nomisCat.nextReviewDate),
-              dateRequiredIsoFormat: nomisCat && nomisCat.nextReviewDate,
-            }
+            sentenceAndDate = { dateRequired: nomisCat && dateConverter(nomisCat.nextReviewDate) }
           }
 
           return {
