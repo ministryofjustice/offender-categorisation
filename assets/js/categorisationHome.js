fa97d1bd4917363d6df8476e9e50f2ff89e83d8b
timothy-ness <160499954+timothy-ness@users.noreply.github.com>
2025-03-06 10:26:20 +0000

    Maintain sort order when applying filters (#915)
    * should maintain sort order when filters are applied
    
    * fixed failing cypress test

--- assets/js/categorisationHome.js
+++ assets/js/categorisationHome.js
@@ -33,15 +33,16 @@ if (hideFilterButton) {
   })
 }
 
+// This is to maintain the sort order when the apply filters button is pressed,
+// it will store the sort attribute and sort order in hidden inputs to be included
+// in the apply filter request
 const applyFiltersButton = document.getElementById('applyFilters')
 if (applyFiltersButton) {
   applyFiltersButton.addEventListener('click', async () => {
-    const table = document.getElementById('offenderTable')
-
-    if (!table) {
+    const table = document.getElementsByClassName('recategorisation-table')[0]
+    if (typeof table === 'undefined') {
       return
     }
-
     const headers = table.getElementsByTagName('th')
     Object.values(headers).forEach(header => {
       const ariaSort = header.getAttribute('aria-sort')
