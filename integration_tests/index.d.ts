ca3c7e2f6d735245e0e164a799dcb3884be3e2e4
Evald <evald.spedzhev@gmail.com>
2025-03-07 13:29:05 +0000

    added some rules to be ignored

--- integration_tests/index.d.ts
+++ integration_tests/index.d.ts
@@ -1,22 +1,24 @@
+/// <reference types="cypress" />
+
 declare namespace Cypress {
   interface Chainable {
     /**
      * Custom command to check a HTML definition list for a given term (dt), and the text value for that definition (dl).
      * @example cy.checkDefinitionList({ term: 'Categoriser comments', definition: 'Comments from Categoriser' })
      */
-    checkDefinitionList({ term: string, definition: string }): Chainable<AUTWindow>
+    checkDefinitionList(params: { term: string; definition: string }): Chainable<AUTWindow>
 
     /**
      * Custom command to check a HTML table for a given header, and all text values in that column.
      * @example cy.checkTableColumnTextValues({ columnName: 'Name', expectedValues: ['Dent, Jane', 'Clark, Frank'] })
      */
-    checkTableColumnTextValues({ columnName: string, expectedValues: string[] }): Chainable<AUTWindow>
+    checkTableColumnTextValues(params: { columnName: string; expectedValues: string[] }): Chainable<AUTWindow>
 
     /**
      * Custom command to check all rows in a HTML table match the provided array of values
      * @example cy.checkTableRowData({ tableRowsSelector: '#initialTable > tbody > tr', expectedValues: [['a','b','c'], ['a','x','y]] })
      */
-    checkTableRowData<T>({ tableRowsSelector: string; expectedValues: T }): Chainable<AUTWindow>
+    checkTableRowData<T>({ tableRowsSelector: string, expectedValues: T }): Chainable<AUTWindow>
 
     /**
      * Custom command to validate the common categorisation details
@@ -44,6 +46,12 @@ declare namespace Cypress {
      * @param retries Number of retries to perform. Default is 10.
      * @param delay Delay between retries in milliseconds. Default is 500ms.
      */
-    assertDBWithRetries(task: string, arg: any, assertionFn: (result: any) => boolean, retries?: number, delay?: number): Chainable<boolean>;
+    assertDBWithRetries(
+      task: string,
+      arg: any,
+      assertionFn: (result: any) => boolean,
+      retries?: number,
+      delay?: number
+    ): Chainable<boolean>
   }
 }
