534406728817b2d7b1697d275386a7918ac72c98
Evald <evald.spedzhev@gmail.com>
2025-03-17 14:12:10 +0000

    SI-1449 fix feature flag factory

--- server/middleware/featureFlag.test-factory.ts
+++ server/middleware/featureFlag.test-factory.ts
@@ -1,7 +1,9 @@
 import { FeatureFlagDto } from './featureFlag.dto'
+import config from '../config'
 
-export const makeTestFeatureFlagDto = (featureFlagDto: Partial<FeatureFlagDto> = {}): FeatureFlagDto => {
-  return featureFlagDto
-}
+export const makeTestFeatureFlagDto = (featureFlagDto: Partial<FeatureFlagDto> = {}): FeatureFlagDto => ({
+  three_to_five_policy_change:
+    featureFlagDto.three_to_five_policy_change ?? config.featureFlags.three_to_five_policy_change,
+})
 
 export default makeTestFeatureFlagDto
