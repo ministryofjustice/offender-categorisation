bd44f2606e834049ce1dd283fdcba2b21295e0a3
Evald <evald.spedzhev@gmail.com>
2025-03-21 09:26:49 +0000

    SI-1505 reverted some changes

--- integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/model/TestFixture.groovy
+++ integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/model/TestFixture.groovy
@@ -1,7 +1,11 @@
 package uk.gov.justice.digital.hmpps.cattool.model
 
 import geb.Browser
-import uk.gov.justice.digital.hmpps.cattool.mockapis.*
+import uk.gov.justice.digital.hmpps.cattool.mockapis.AllocationApi
+import uk.gov.justice.digital.hmpps.cattool.mockapis.Elite2Api
+import uk.gov.justice.digital.hmpps.cattool.mockapis.OauthApi
+import uk.gov.justice.digital.hmpps.cattool.mockapis.PrisonerSearchApi
+import uk.gov.justice.digital.hmpps.cattool.mockapis.RiskProfilerApi
 import uk.gov.justice.digital.hmpps.cattool.pages.CategoriserHomePage
 import uk.gov.justice.digital.hmpps.cattool.pages.recat.RecategoriserHomePage
 
@@ -10,7 +14,10 @@ import java.time.LocalDate
 import java.time.temporal.ChronoField
 import java.time.temporal.ChronoUnit
 
-import static uk.gov.justice.digital.hmpps.cattool.model.UserAccount.*
+import static uk.gov.justice.digital.hmpps.cattool.model.UserAccount.CATEGORISER_USER
+import static uk.gov.justice.digital.hmpps.cattool.model.UserAccount.FEMALE_RECAT_USER
+import static uk.gov.justice.digital.hmpps.cattool.model.UserAccount.RECATEGORISER_USER
+import static uk.gov.justice.digital.hmpps.cattool.model.UserAccount.FEMALE_USER
 
 class TestFixture {
 
