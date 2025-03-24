f1e0b6255474508cf027d4a61a9b2f780e5e36b9
Evald <evald.spedzhev@gmail.com>
2025-03-24 17:17:19 +0000

    SI-1505 PR comments

--- integration_tests/pages/form/openConditions/foreignNational.ts
+++ integration_tests/pages/form/openConditions/foreignNational.ts
@@ -68,7 +68,7 @@ export default class ForeignNationalPage extends Page {
   validateErrorSummaryMessages(
     errorSummaryMessages: {
       index: number
-      href: ForeignNationalChoiceValues | string
+      href: ForeignNationalChoiceValues | HomeOfficeImmigrationStatusChoiceValues | ExhaustedAppealChoiceValues
       text: string
     }[]
   ) {
