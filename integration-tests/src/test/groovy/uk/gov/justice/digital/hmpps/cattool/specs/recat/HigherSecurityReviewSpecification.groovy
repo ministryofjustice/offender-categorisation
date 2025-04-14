92f71e996ace951059fbbbecef411fb92cd5a7be
Evald <evald.spedzhev@gmail.com>
2025-04-14 10:00:15 +0100

    flaky text fix

--- integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/specs/recat/HigherSecurityReviewSpecification.groovy
+++ integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/specs/recat/HigherSecurityReviewSpecification.groovy
@@ -19,8 +19,10 @@ class HigherSecurityReviewSpecification extends AbstractSpecification {
 
     then: 'I stay on the page with validation errors'
     at HigherSecurityReviewPage
-    errorSummaries*.text() == ['Please enter behaviour details', 'Please enter steps details', 'Please select yes or no', 'Please enter security conditions details']
-    errors*.text() == ['Error:\nPlease enter details', 'Error:\nPlease enter details', 'Error:\nPlease select yes or no', 'Error:\nPlease enter details']
+    waitFor {
+      errorSummaries*.text() == ['Please enter behaviour details', 'Please enter steps details', 'Please select yes or no', 'Please enter security conditions details']
+      errors*.text() == ['Error:\nPlease enter details', 'Error:\nPlease enter details', 'Error:\nPlease select yes or no', 'Error:\nPlease enter details']
+    }
 
     when: 'I click no but fail to add details'
     transferNo.click()
