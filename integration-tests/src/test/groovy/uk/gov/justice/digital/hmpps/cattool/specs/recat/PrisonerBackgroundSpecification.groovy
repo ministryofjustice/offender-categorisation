e2b9b4d699b9b57b9a562afd80946f2a762357f7
Charlotte l'Anson <charlotte.ianson@justice.gov.uk>
2025-07-01 10:05:30 +0100

    fix cypress test and remove comment assertion in groovy test

--- integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/specs/recat/PrisonerBackgroundSpecification.groovy
+++ integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/specs/recat/PrisonerBackgroundSpecification.groovy
@@ -25,10 +25,6 @@ class PrisonerBackgroundSpecification extends AbstractSpecification {
     then: 'The page is displayed'
     at PrisonerBackgroundPage
     headerValue*.text() == fixture.MINI_HEADER
-    alertInfo*.text() == [
-      'E-List: First xel comment 2016-09-14',
-      '''E-List: Second xel comment with lengthy text comment with lengthy text comment with lengthy text comment with lengthy text comment with lengthy text comment with lengthy text comment with lengthy text comment with lengthy text comment with lengthy text comment with lengthy text comment with lengthy text comment with lengthy text comment with lengthy text 2016-09-15 (expired) (inactive)''',
-      'Escape Risk Alert: First xer comment 2016-09-16']
     extremismWarning.text() contains 'This person is at risk of engaging in, or vulnerable to, extremism'
     violenceWarning.text() contains 'This person has been reported as the perpetrator in 5 assaults in custody before, including 2 serious assaults and 3 non-serious assaults in the past 12 months.'
     !violenceNotifyWarning.displayed
