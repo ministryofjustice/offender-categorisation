76a342d29b9534f9087a05e5c8e2a919069037f5
Charlotte l'Anson <charlotte.ianson@justice.gov.uk>
2025-07-09 16:04:25 +0100

    run groovy tests

--- integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/mockapis/PathfinderApi.groovy
+++ integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/mockapis/PathfinderApi.groovy
@@ -7,16 +7,17 @@ import static com.github.tomakehurst.wiremock.client.WireMock.aResponse
 import static com.github.tomakehurst.wiremock.client.WireMock.get
 
 class PathfinderApi extends WireMockRule {
-    
-    PathfinderApi() {
-        super(8081);
-    }
 
-    void stubGetExtremismProfile(String offenderNo, Number band) {
-        this.stubFor(get("/pathfinder/offender/" + offenderNo)
-        .willReturn(aResponse()
+  PathfinderApi() {
+    super(8081);
+  }
+
+  void stubGetExtremismProfile(String offenderno, Number band) {
+    this.stubFor(get("/pathfinder/offender/${offenderno}")
+      .willReturn(aResponse()
         .withStatus(200)
-        .withHeader('Content-Type', 'application/json')
-        .withBody(JsonOutput.toJson([band : band]))))
-    }
+        .withHeader("Content-Type", "application/json")
+        .withBody(JsonOutput.toJson([band : band]))
+      ))
+  }
 }
