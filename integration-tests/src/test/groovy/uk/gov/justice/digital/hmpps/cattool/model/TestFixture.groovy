58751bd8dd3050865b5512386ab2c46989955094
timothy-ness <timothy.ness@digital.justice.gov.uk>
2025-07-15 14:49:13 +0100

    added in parameter which was missing

--- integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/model/TestFixture.groovy
+++ integration-tests/src/test/groovy/uk/gov/justice/digital/hmpps/cattool/model/TestFixture.groovy
@@ -4,6 +4,7 @@ import geb.Browser
 import uk.gov.justice.digital.hmpps.cattool.mockapis.AllocationApi
 import uk.gov.justice.digital.hmpps.cattool.mockapis.Elite2Api
 import uk.gov.justice.digital.hmpps.cattool.mockapis.OauthApi
+import uk.gov.justice.digital.hmpps.cattool.mockapis.PathfinderApi
 import uk.gov.justice.digital.hmpps.cattool.mockapis.PrisonerSearchApi
 import uk.gov.justice.digital.hmpps.cattool.mockapis.RiskProfilerApi
 import uk.gov.justice.digital.hmpps.cattool.pages.CategoriserHomePage
@@ -27,6 +28,7 @@ class TestFixture {
   AllocationApi allocationApi
   PrisonerSearchApi prisonerSearchApi
   OauthApi oauthApi
+  PathfinderApi pathfinderApi
 
   UserAccount currentUser
 
@@ -163,13 +165,22 @@ class TestFixture {
   public static final MINI_HEADER1 = ['Hillmob, William', 'ON700', '17/02/1970', 'Closed']
 
 
-  TestFixture(Browser browser, Elite2Api elite2Api, OauthApi oauthApi, RiskProfilerApi riskProfilerApi1, AllocationApi allocationApi1, PrisonerSearchApi prisonerSearchApi) {
+  TestFixture(
+    Browser browser,
+    Elite2Api elite2Api,
+    OauthApi oauthApi,
+    RiskProfilerApi riskProfilerApi1,
+    AllocationApi allocationApi1,
+    PrisonerSearchApi prisonerSearchApi,
+    PathfinderApi pathfinderApi
+  ) {
     this.browser = browser
     this.elite2Api = elite2Api
     this.riskProfilerApi = riskProfilerApi1
     this.allocationApi = allocationApi1
     this.prisonerSearchApi = prisonerSearchApi
     this.oauthApi = oauthApi
+    this.pathfinderApi = pathfinderApi
   }
 
   def loginAs(UserAccount user) {
