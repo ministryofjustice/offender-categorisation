2d16b3c4db41f31fbe4a09540f176b539311a612
timothy-ness <timothy.ness@digital.justice.gov.uk>
2025-12-02 09:04:00 +0000

    updated allowed scripts with cypress

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,5 +2,9 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
    allowlist: {
+     // Needed by esbuild for watching files during development
+     'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
+    // Needed for running integration tests:
+    'node_modules/cypress@15.5.0': 'ALLOW',
    },
 })
