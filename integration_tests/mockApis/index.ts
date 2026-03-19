c14cf9c13a1ba06687cd7b471dcb90218afda191
timothy-ness <timothy.ness@digital.justice.gov.uk>
2026-03-19 08:46:10 +0000

    removing risk profiler references

--- integration_tests/mockApis/index.ts
+++ integration_tests/mockApis/index.ts
@@ -2,7 +2,6 @@ import allocationManager from './allocationManager'
 import auth from './auth'
 import elite2 from './elite2'
 import prisonerSearch from './prisonerSearch'
-import riskProfiler from './riskProfiler'
 import tokenVerification from './tokenVerification'
 import dpsComponents from './dpsComponents'
 import probationOffenderSearchApi from './probationOffenderSearchApi'
@@ -16,7 +15,6 @@ export default {
   ...auth,
   ...elite2,
   ...prisonerSearch,
-  ...riskProfiler,
   ...tokenVerification,
   ...dpsComponents,
   ...probationOffenderSearchApi,
