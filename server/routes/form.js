0d0b74dde89c4cd5d7c861dfc9c987b8cb3fd468
timothy-ness <timothy.ness@digital.justice.gov.uk>
2026-09-11 11:13:07 +0100

    added log

--- server/routes/form.js
+++ server/routes/form.js
@@ -354,6 +354,7 @@ module.exports = function Index({
       : await formService.getCategorisationRecord(bookingId, transactionalDbClient)
 
     if (!formData || !formData.formObject) {
+      log.info(formData)
       throw new Error('No categorisation found for this booking id / sequence no')
     }
     res.locals.formObject = { ...formData.formObject, ...formData.riskProfile }
