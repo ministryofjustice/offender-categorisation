fa9d2598bfcb004d2e0951b04e7a3da41855cb41
timothy-ness <timothy.ness@digital.justice.gov.uk>
2026-09-23 14:12:35 +0100

    fix for lint

--- assets/js/govukInitialisation.js
+++
@@ -1,9 +0,0 @@
-import * as GOVUKFrontend from '/assets/govuk/govuk-frontend.min.js'
-import * as MOJFrontend from '/assets/moj/moj-frontend.min.js'
-
-// expose for legacy code
-window.GOVUKFrontend = GOVUKFrontend
-window.MOJFrontend = MOJFrontend
-
-GOVUKFrontend.initAll()
-MOJFrontend.initAll()
