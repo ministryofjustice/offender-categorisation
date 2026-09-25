04b8d547b0e25964b035af2443211da730896fdd
timothy-ness <160499954+timothy-ness@users.noreply.github.com>
2026-09-25 08:59:36 +0100

    Si 1602 update mojfrontend (#1063)
    * updated mojfrontend to version 4
    
    * fix for lint
    
    * updated mojfrontend to version 5
    
    * fix for cypress test
    
    * updated flakey test
    
    * updated to mojfrontend v6
    
    * updated to mojfrontend v7
    
    * updated to mojfrontend v9
    
    * update importing of scss
    
    * update to mojfrontend 11

--- assets/js/govukInitialisation.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line import/no-unresolved,import/extensions,import/no-absolute-path
-import { initAll } from '/assets/govuk/govuk-frontend.min.js'
-
-initAll()
-window.MOJFrontend.initAll()
